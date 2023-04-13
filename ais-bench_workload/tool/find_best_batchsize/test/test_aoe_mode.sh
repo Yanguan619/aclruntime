1493c161b6db85960e17763ffd013c788d556cd2
yanhe13 <1037452625@qq.com>
2023-04-13 20:18:53 +0800

    bugfix0.01

--- ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
+++ ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
@@ -8,7 +8,7 @@
 # 功能测试 
 echo "test: model_type onnx, aoe_mode on, max_batch_num = 64"
 echo "to test aoe mode for onnx model"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
@@ -22,7 +22,7 @@ $cmd
 
 echo "test: model_type onnx, aoe_mode off, max_batch_num = 4"
 echo "to test atc mode still avaliable"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
@@ -36,7 +36,7 @@ $cmd
 
 echo "test: model_type pb, aoe_mode on, max_batch_num = 8"
 echo "to test aoe mode for tf model"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/tf_MDL/ResNet50/tf_resnet50.pb
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
@@ -50,7 +50,7 @@ $cmd
 
 echo "test: model_type caffemodel, aoe_mode on, max_batch_num = 8"
 echo "to test aoe mode for caffe model"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.prototxt
     --weight_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.caffemodel
     --python_command python3.8 
@@ -66,7 +66,7 @@ $cmd
 # 异常情况测试
 echo "test: model_type onnx, aoe_mode = 3, max_batch_num = 8"
 echo "to test illegal aoe_mode input"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
@@ -80,7 +80,7 @@ $cmd
 
 echo "test: model_type onnx, job_type = 3, max_batch_num = 8"
 echo "to test illegal job_type input"
-cmd="bash /../find_best_batchsize.sh 
+cmd="bash ../find_best_batchsize.sh 
     --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244

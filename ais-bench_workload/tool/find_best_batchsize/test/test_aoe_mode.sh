f8b327bff713aa14c4b3b3c2c0c4ebae7edf15de
yanhe13 <1037452625@qq.com>
2023-04-13 20:06:21 +0800

    add README.md of test_aoe_mode.sh, 实例化test_aoe_mode.sh

--- ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
+++ ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
@@ -8,8 +8,8 @@
 # 功能测试 
 echo "test: model_type onnx, aoe_mode on, max_batch_num = 64"
 echo "to test aoe mode for onnx model"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .onnx
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310
@@ -22,8 +22,8 @@ $cmd
 
 echo "test: model_type onnx, aoe_mode off, max_batch_num = 4"
 echo "to test atc mode still avaliable"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .onnx
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310
@@ -36,8 +36,8 @@ $cmd
 
 echo "test: model_type pb, aoe_mode on, max_batch_num = 8"
 echo "to test aoe mode for tf model"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .pb
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/tf_MDL/ResNet50/tf_resnet50.pb
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310
@@ -50,9 +50,9 @@ $cmd
 
 echo "test: model_type caffemodel, aoe_mode on, max_batch_num = 8"
 echo "to test aoe mode for caffe model"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .prototxt
-    --weight_path .caffemodel
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.prototxt
+    --weight_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.caffemodel
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310
@@ -66,8 +66,8 @@ $cmd
 # 异常情况测试
 echo "test: model_type onnx, aoe_mode = 3, max_batch_num = 8"
 echo "to test illegal aoe_mode input"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .onnx
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310
@@ -80,8 +80,8 @@ $cmd
 
 echo "test: model_type onnx, job_type = 3, max_batch_num = 8"
 echo "to test illegal job_type input"
-cmd="bash /../find_best_batchsie.sh 
-    --model_path .onnx
+cmd="bash /../find_best_batchsize.sh 
+    --model_path /home/y30044005/onnx_MDL/ResNeSt50/resnest50.onnx
     --python_command python3.8 
     --input_shape_str actual_input_1:batchsize,3,224,244
     --soc_version Ascend310

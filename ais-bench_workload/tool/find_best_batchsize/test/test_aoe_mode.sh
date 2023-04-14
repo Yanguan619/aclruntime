5caa2ad12b97965a1d7dd4bf1707dce5d0ef824a
yanhe13 <1037452625@qq.com>
2023-04-14 11:02:47 +0800

    bugfix0.021

--- ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
+++ ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
@@ -41,39 +41,39 @@ cmd="rm -rf ../cache/"
 $cmd
 
 
-echo "test: model_type pb, aoe_mode on, max_batch_num = 8"
-echo "to test aoe mode for tf model"
-cmd="bash ../find_best_batchsize.sh 
-    --model_path /home/y30044005/tf_MDL/ResNet50/tf_resnet50.pb
-    --python_command python3.8 
-    --input_shape_str actual_input_1:batchsize,3,224,244
-    --soc_version Ascend310
-    --loop_count 100
-    --max_batch_num 8
-    --aoe_mode 1
-    --job_type 1
-    "
-$cmd
-cmd="rm -rf ../cache/"
-$cmd
+# echo "test: model_type pb, aoe_mode on, max_batch_num = 8"
+# echo "to test aoe mode for tf model"
+# cmd="bash ../find_best_batchsize.sh 
+#     --model_path /home/y30044005/tf_MDL/ResNet50/tf_resnet50.pb
+#     --python_command python3.8 
+#     --input_shape_str actual_input_1:batchsize,3,224,244
+#     --soc_version Ascend310
+#     --loop_count 100
+#     --max_batch_num 8
+#     --aoe_mode 1
+#     --job_type 1
+#     "
+# $cmd
+# cmd="rm -rf ../cache/"
+# $cmd
 
 
-echo "test: model_type caffemodel, aoe_mode on, max_batch_num = 8"
-echo "to test aoe mode for caffe model"
-cmd="bash ../find_best_batchsize.sh 
-    --model_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.prototxt
-    --weight_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.caffemodel
-    --python_command python3.8 
-    --input_shape_str actual_input_1:batchsize,3,224,244
-    --soc_version Ascend310
-    --loop_count 100
-    --max_batch_num 64
-    --aoe_mode 1
-    --job_type 1
-    "
-$cmd
-cmd="rm -rf ../cache/"
-$cmd
+# echo "test: model_type caffemodel, aoe_mode on, max_batch_num = 8"
+# echo "to test aoe mode for caffe model"
+# cmd="bash ../find_best_batchsize.sh 
+#     --model_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.prototxt
+#     --weight_path /home/y30044005/caffe_MDL/ResNet50/ResNet50.caffemodel
+#     --python_command python3.8 
+#     --input_shape_str actual_input_1:batchsize,3,224,244
+#     --soc_version Ascend310
+#     --loop_count 100
+#     --max_batch_num 64
+#     --aoe_mode 1
+#     --job_type 1
+#     "
+# $cmd
+# cmd="rm -rf ../cache/"
+# $cmd
 
 
 # 异常情况测试

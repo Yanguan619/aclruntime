c8686aa82463986ee9ecdca98ab9b47b0466d5e8
yanhe13 <1037452625@qq.com>
2023-04-14 10:27:01 +0800

    bugfix0.1

--- ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
+++ ais-bench_workload/tool/find_best_batchsize/test/test_aoe_mode.sh
@@ -20,7 +20,7 @@ cmd="bash ../find_best_batchsize.sh
     --aoe_mode 1
     --job_type 1
     "
-# $cmd
+$cmd
 cmd="rm -rf ../cache/"
 $cmd
 
@@ -36,7 +36,7 @@ cmd="bash ../find_best_batchsize.sh
     --aoe_mode 0
     --job_type 1
     "
-# $cmd
+$cmd
 cmd="rm -rf ../cache/"
 $cmd
 
@@ -53,7 +53,7 @@ cmd="bash ../find_best_batchsize.sh
     --aoe_mode 1
     --job_type 1
     "
-[[ $cmd != $ret_ok ]] || { echo "command failed";exit 1; }
+$cmd
 cmd="rm -rf ../cache/"
 $cmd
 
@@ -71,7 +71,7 @@ cmd="bash ../find_best_batchsize.sh
     --aoe_mode 1
     --job_type 1
     "
-[[ $cmd ]] || { echo "command failed";exit 1; }
+$cmd
 cmd="rm -rf ../cache/"
 $cmd
 

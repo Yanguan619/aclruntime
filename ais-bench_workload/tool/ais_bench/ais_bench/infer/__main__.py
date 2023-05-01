10d2e968f507a83e4b5b8b708e624f92ceddad37
JacobHuang <huangjianbin1@huawei.com>
2023-05-01 03:12:46 +0000

    update ais-bench_workload/tool/ais_bench/ais_bench/infer/__main__.py.
    Signed-off-by: JacobHuang <huangjianbin1@huawei.com>

--- ais-bench_workload/tool/ais_bench/ais_bench/infer/__main__.py
+++ ais-bench_workload/tool/ais_bench/ais_bench/infer/__main__.py
@@ -281,11 +281,12 @@ def main(args, index=0, msgq=None):
     intensors_desc = session.get_inputs()
 
     if args.output != None:
+        timestr = time.strftime("%Y_%m_%d-%H_%M_%S")
+        output_prefix = os.path.join(args.output, timestr)
         if args.output_dirname is None:
-            timestr = time.strftime("%Y_%m_%d-%H_%M_%S")
-            output_prefix = os.path.join(args.output, timestr)
+            output_prefix = os.path.join(output_prefix, "dev".format(args.device))
         else:
-            output_prefix = os.path.join(args.output, args.output_dirname)
+            output_prefix = os.path.join(output_prefix, args.output_dirname, "dev".format(args.device))
         if not os.path.exists(output_prefix):
             os.makedirs(output_prefix, 0o755)
         logger.info("output path:{}".format(output_prefix))
@@ -359,8 +360,6 @@ def multidevice_run(args):
     for i in range(len(device_list)):
         cur_args = copy.deepcopy(args)
         cur_args.device = int(device_list[i])
-        if args.output != None:
-            cur_args.output = "{}dev{}".format(args.output, cur_args.device)
         p.apply_async(main, args=(cur_args, i, msgq), error_callback=print_subproces_run_error)
 
     p.close()

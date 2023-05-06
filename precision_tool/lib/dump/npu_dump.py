9749b9ab1a9473cf999e73cab63388fc25753d76
suyueming <suyueming@huawei.com>
2023-05-06 10:42:47 +0800

    fix dump csv decode bugs

--- precision_tool/lib/dump/npu_dump.py
+++ precision_tool/lib/dump/npu_dump.py
@@ -104,8 +104,8 @@ class NpuDump(object):
         for npu_dump_file in npu_dump_files:
             if str(npu_dump_file.file_name).endswith(Constant.Suffix.CSV):
                 detail = util.read_csv(npu_dump_file.path)
-                input_txt.append(' -%s' % (npu_dump_file.idx, npu_dump_file.file_name))
-                output_txt.append(' -%s' % (npu_dump_file.idx, npu_dump_file.file_name))
+                input_txt.append(' -[%s]%s' % (npu_dump_file.idx, npu_dump_file.file_name))
+                output_txt.append(' -[%s]%s' % (npu_dump_file.idx, npu_dump_file.file_name))
                 for item in detail:
                     item_txt = '[Shape: %s] [Dtype: %s] [Max: %s] [Min: %s] [Mean: %s]' % (item[5], item[3], item[6], item[7], item[8])
                     if item[0] == 'Input':
@@ -134,7 +134,7 @@ class NpuDump(object):
         self.dump_files = util.list_npu_dump_files(sub_dir)
 
     def list_dump(self, dir_path, file_name):
-        """"""
+        """list dump"""
 
     @staticmethod
     def get_npu_dump_decode_files_by_name(file_name):

ade2dc7e4d372651eb97fe372baa1fc18d7b02e1
王守策 <wangshouce.827023@huawei.com>
2023-06-06 07:51:12 +0000

    update ptdbg_ascend/src/python/ptdbg_ascend/overflow_check/overflow_check.py.
    Signed-off-by: 王守策 <wangshouce.827023@huawei.com>

--- ptdbg_ascend/src/python/ptdbg_ascend/overflow_check/overflow_check.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/overflow_check/overflow_check.py
@@ -4,10 +4,10 @@ from ..common.utils import print_warn_log, get_time, print_info_log
 from ..dump.dump import forward_init_status, forward_acl_dump
 from .utils import OverFlowUtil, dump_overflow
 from ..dump.utils import DumpUtil
-import torch_npu
+import torch
 
 try:
-    import torch
+    import torch_npu
 except ImportError:
     is_gpu = True
 else:

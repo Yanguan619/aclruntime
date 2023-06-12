89daf97d2830ce961400d2b7be38aaa41478b605
h00613304 <hekunkun@huawei.com>
2023-06-12 11:57:48 +0800

    添加对==运算符的dump

--- ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
@@ -70,10 +70,7 @@ def get_not_float_tensor_info(data):
 
 def get_scalar_data_info(data):
     summary_data = []
-    if isinstance(data, slice):
-        summary_data = [[], [], []]
-    else:
-        summary_data.extend([data, data, data])
+    summary_data.extend([data, data, data])
     return DataInfo(data, data, summary_data, str(type(data)), str([]))
 
 

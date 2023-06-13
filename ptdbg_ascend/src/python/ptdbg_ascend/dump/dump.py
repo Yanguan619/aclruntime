ab115a446f7e245be2d693c85ccceb456c344c14
h00613304 <hekunkun@huawei.com>
2023-06-13 12:00:30 +0800

    修复api_stack模式下无法删除同名pkl文件的问题

--- ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
@@ -32,7 +32,7 @@ else:
 
 from .utils import DumpUtil, _set_dump_switch4api_list, make_dump_data_dir
 
-from ..common.utils import print_warn_log, Const, print_info_log
+from ..common.utils import print_warn_log, Const, print_info_log, modify_dump_path
 
 forward_init_status = False
 backward_init_status = False
@@ -163,6 +163,8 @@ def dump_api_tensor(dump_step, in_feat, name_template, out_feat, dump_file):
 def dump_acc_cmp(name, in_feat, out_feat, dump_step, module):
     dump_file = DumpUtil.get_dump_path()
     _set_dump_switch4api_list(name)
+    if DumpUtil.dump_switch_mode == Const.API_STACK:
+        dump_file = modify_dump_path(dump_file)
 
     if DumpUtil.get_dump_switch():
         if DumpUtil.dump_init_enable:

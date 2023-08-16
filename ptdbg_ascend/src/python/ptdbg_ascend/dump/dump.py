c5d40a2b8107447022aaa27811a8400ee621a1fb
litian_drinksnow <litian68@huawei.com>
2023-08-16 10:29:35 +0800

    fix typo

--- ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/dump/dump.py
@@ -183,8 +183,8 @@ def dump_api_tensor(dump_step, in_feat, name_template, out_feat, dump_file):
 def dump_acc_cmp(name, in_feat, out_feat, dump_step, module):
     dump_file = DumpUtil.get_dump_path()
     dump_file = modify_dump_path(dump_file, DumpUtil.dump_switch_mode)
+    _set_dump_switch4api_list(name)
     if DumpUtil.get_dump_switch():
-        _set_dump_switch4api_list(name)
         rank = get_tensor_rank(in_feat, out_feat)
         if DumpUtil.target_rank is not None:
             if rank != DumpUtil.target_rank:

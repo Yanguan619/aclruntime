13e71ef4d127c758819d08eee14b4328985da31d
jiangchangting1 <jiangchangting@huawei.com>
2023-08-21 11:52:46 +0000

    update ptdbg_ascend/src/python/ptdbg_ascend/common/utils.py.
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- ptdbg_ascend/src/python/ptdbg_ascend/common/utils.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/common/utils.py
@@ -268,11 +268,11 @@ def check_dump_mode_valid(dump_mode):
         return ['all']
     return dump_mode
 
-def check_pkl_only_valid(pkl_only):
-    if not isinstance(pkl_only, bool):
+def check_summary_only_valid(summary_only):
+    if not isinstance(summary_only, bool):
         print_error_log("Params auto_analyze only support True or False.")
         raise CompareException(CompareException.INVALID_PARAM_ERROR)
-    return pkl_only
+    return summary_only
 
 def check_compare_param(input_parma, output_path, stack_mode=False, auto_analyze=True,
                         fuzzy_match=False):  # 添加默认值来让不传参时能通过参数检查

4c85edac195d4489451b423695c57343c08fc03a
yanhe13 <yanhe13@huawei.com>
2023-11-25 09:39:14 +0800

    llama workload debug v15

--- ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/pre_conf_yaml.py
+++ ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/pre_conf_yaml.py
@@ -27,7 +27,7 @@ else:
 if os.getenv('LLAMA_RUN_MODE') == 'only_finetune':
     ckpt_path = os.path.join(cur_path, f'datas/open_llama_{model_type}')
 else:
-    ckpt_path = os.path.join(cur_path, f'datas/target_ckpt/llama_{model_type}0.ckpt')
+    ckpt_path = os.path.join(cur_path, f'datas/target_ckpt/rank_0/llama_{model_type}0.ckpt')
 
 if not os.path.exists(target_yaml):
     raise RuntimeError(f"yaml file: {target_yaml} not find!")

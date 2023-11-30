a090c1fb2e0ebc7589da2744ea3910df63fc06b1
yanhe13 <yanhe13@huawei.com>
2023-11-30 15:48:31 +0800

    llama workload debug v35

--- ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/pre_conf_yaml.py
+++ ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/pre_conf_yaml.py
@@ -47,7 +47,7 @@ def write_pretrain_yaml(data):
     data['train_dataset']['data_loader']['dataset_dir'] = pretrain_dataset
     data['eval_dataset']['data_loader']['dataset_dir'] = eval_data_path
     data['model']['model_config']['num_layers'] = int(layer_num)
-    data['callbacks']['save_checkpoint_steps'] = 100000
+    data['callbacks'][1]['save_checkpoint_steps'] = 100000
     return data
 
 
@@ -64,7 +64,7 @@ def write_finetune_yaml(data):
     data['train_dataset']['data_loader']['dataset_dir'] = finetune_dataset
     data['eval_dataset']['data_loader']['dataset_dir'] = eval_data_path
     data['model']['model_config']['num_layers'] = int(layer_num)
-    data['callbacks']['save_checkpoint_steps'] = 100000
+    data['callbacks'][1]['save_checkpoint_steps'] = 100000
     return data
 
 data = {}

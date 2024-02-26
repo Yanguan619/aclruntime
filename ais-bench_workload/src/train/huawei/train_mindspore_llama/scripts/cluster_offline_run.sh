00ea8b6e96471c53dceb699e66f11884404f5c23
yanhe13 <yanhe13@huawei.com>
2024-02-26 10:07:53 +0800

    multi nodes finetune modifyv2

--- ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/cluster_offline_run.sh
+++ ais-bench_workload/src/train/huawei/train_mindspore_llama/scripts/cluster_offline_run.sh
@@ -68,13 +68,14 @@ init()
     rm -rf ${BASE_PATH}/result;mkdir -p ${BASE_PATH}/result
     rm -rf $RESULT_PATH;mkdir -p $RESULT_PATH
     rm -rf $WORK_PATH;mkdir -p $WORK_PATH
-    cp -r $CODE_PATH/* $WORK_PATH # CPU可以执行的都在host节点执行
-
     if [ "$NODEINFO_FILE" != "" ];then
-         # sync data if work_path not exist so new one.节点的work/ 路径是相对于在node_file中指定的work_path
         cmd="rm -rf ${RELAT_WORK_PATH};mkdir -p ${RELAT_WORK_PATH}"
         cluster_multi_exec "$cmd" serial || { logger_Error "renew workpath failed"; return 1; }
          # copy code to node work path
+    fi
+    cp -r $CODE_PATH/* $WORK_PATH # CPU可以执行的都在host节点执行
+    if [ "$NODEINFO_FILE" != "" ];then
+         # sync data if work_path not exist so new one.节点的work/ 路径是相对于在node_file中指定的work_path
         cluster_multi_put "$WORK_PATH" "./"  || { logger_Error "deploy code to work place failed"; return 1; }
     fi
     cmd="source /etc/profile;

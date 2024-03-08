38ccc62051b8fe6644bc6d6d67ea3c3fcf609545
yanhe13 <yanhe13@huawei.com>
2024-03-08 15:13:57 +0800

    add aclruntime version description

--- ais-bench_workload/tool/ais_bench/setup.py
+++ ais-bench_workload/tool/ais_bench/setup.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import subprocess
-import re
 from setuptools import setup, find_packages  # type: ignore
 
 

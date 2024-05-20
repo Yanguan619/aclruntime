52d9fe518094ad2efdb3d5ea5871e3a49b9ebede
yanhe13 <yanhe13@huawei.com>
2024-05-20 15:43:16 +0800

    readme explain obs

--- ais-bench_workload/tool/ais_bench/backend/setup.py
+++ ais-bench_workload/tool/ais_bench/backend/setup.py
@@ -197,7 +197,7 @@ setup(
     version=STATIC_VERSION,
     author="ais_bench",
     author_email="aclruntime",
-    url=f"commit id: {git_hash}, release_date: {git_date}",
+    url=f"gitee repo: Ascend/tools, commit id: {git_hash}, release_date: {git_date}",
     release_date = git_date,
     description="A test project using pybind11 and aclruntime",
     long_description="",

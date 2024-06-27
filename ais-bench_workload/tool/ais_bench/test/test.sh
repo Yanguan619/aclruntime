fe9b621ac8d12ae05ad7a8ea29bc17a571c10cb4
yanhe13 <yanhe13@huawei.com>
2024-06-27 09:15:13 +0800

    dt framework simp

--- ais-bench_workload/tool/ais_bench/test/test.sh
+++ ais-bench_workload/tool/ais_bench/test/test.sh
@@ -22,20 +22,13 @@ CUR_PATH=$(dirname $(readlink -f "$0"))
 set -x
 set -e
 MSAME_PATH=$CUR_PATH/msame
-function get_msame_file()
+SOC_VERSION=""
+
+function get_npu_type()
 {
-    get_arch=`arch`
-    if [[ $get_arch =~ "x86_64" ]];then
-        echo "arch x86_64"
-        local convert_url="https://aisbench.obs.myhuaweicloud.com/packet/msame/x86/msame"
-        wget $convert_url -O $1 --no-check-certificate
-    elif [[ $get_arch =~ "aarch64" ]];then
-        echo "arch arm64"
-        local convert_url="https://aisbench.obs.myhuaweicloud.com/packet/msame/arm/msame"
-        wget $convert_url -O $1 --no-check-certificate
-    else
-        echo "unknown!!"l
-    fi
+    SOC_VERSION=`python3 -c 'import acl; print(acl.get_soc_name())'` || { return $ret_failed; }
+    echo "npu is $SOC_VERSION"
+    return $ret_ok
 }
 
 function chmod_file_data()
@@ -44,43 +37,38 @@ function chmod_file_data()
     chmod -R 750 $CUR_PATH/aipp_config_files
 }
 
+function data_generate()
+{ # all generated data in $CUR_PATH/testdata
+    py_cmd=$1
+    bash -x $CUR_PATH/get_pth_resnet50_data.sh $SOC_VERSION $py_cmd $AISBENCH_INFER_DT_MODE
+    bash -x $CUR_PATH/get_add_model_data.sh
+    ${py_cmd} $CUR_PATH/generate_pipeline_datasets.py
+}
+
 main() {
     chmod_file_data
+    # utils.sh func
+    get_msame_file $MSAME_PATH || { echo "get msame bin file failed";return $ret_failed; }
+    [ -f $MSAME_PATH ] || { echo "not find msame:$MSAME_PATH please check"; return $ret_invalid_args; }
+    chmod 750 $MSAME_PATH
+
     if [ $# -lt 2 ]; then
         echo "at least one parameter. for example: bash test.sh Ascend310P3 python3"
         return $ret_invalid_args
     fi
 
-    export SOC_VERSION=${1:-"Ascend310P3"}
-    export PYTHON_COMMAND=${2:-"python3"}
-    export BENCKMARK_DT_MODE=${3:-"simple"}
+    export PYTHON_COMMAND=${1:-"python3"}
+    export AISBENCH_INFER_DT_MODE=${2:-"simple"}
     export PYTHONPATH=$CUR_PATH:$PYTHONPATH
-
-    get_msame_file $MSAME_PATH || { echo "get msame bin file failed";return 1; }
-    chmod 750 $MSAME_PATH
-    # export MSAME_BIN_PATH=$CUR_PATH/../../../../../../tools/msame/out/msame
     export MSAME_BIN_PATH=$MSAME_PATH
-    [ -f $MSAME_BIN_PATH ] || { echo "not find msame:$MSAME_BIN_PATH please check"; return $ret_invalid_args; }
 
+    # utils.sh func
     check_python_package_is_install $PYTHON_COMMAND "aclruntime" || {
         echo "aclruntime package install failed please install or source set_env.sh"
         return $ret_invalid_args
     }
-    if [ "$AIT_BENCHMARK_DT_DATA_PATH" == "" ]; then
-        bash -x $CUR_PATH/get_pth_resnet50_data.sh $SOC_VERSION $PYTHON_COMMAND $BENCKMARK_DT_MODE
-        bash -x $CUR_PATH/get_add_model_data.sh
-    else
-        echo "using $AIT_BENCHMARK_DT_DATA_PATH as dt data path"
-    fi
-    #bash -x $CUR_PATH/get_pth_resnet101_data.sh $SOC_VERSION $PYTHON_COMMAND
-    #bash -x $CUR_PATH/get_pth_inception_v3_data.sh $SOC_VERSION $PYTHON_COMMAND
-    ${PYTHON_COMMAND} $CUR_PATH/generate_pipeline_datasets.py
 
-    if [ $BENCKMARK_DT_MODE == "full" ];then
-        bash -x $CUR_PATH/get_bert_data.sh $SOC_VERSION $PYTHON_COMMAND
-        bash -x $CUR_PATH/get_yolo_data.sh $SOC_VERSION $PYTHON_COMMAND
-        bash -x $CUR_PATH/get_pth_crnn_data.sh $SOC_VERSION $PYTHON_COMMAND
-    fi
+    data_generate $PYTHON_COMMAND
 
     if [ $BENCKMARK_DT_MODE == "full" ];then
         echo "run DT in full mode"
--- ais-bench_workload/tool/ais_bench/test/utils.sh
+++ ais-bench_workload/tool/ais_bench/test/utils.sh
@@ -13,14 +13,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+declare -i ret_ok=0
+declare -i ret_failed=1
 
-check_python_package_is_install() {
+
+function check_python_package_is_install()
+{
     local PYTHON_COMMAND=$1
     ${PYTHON_COMMAND} -c "import $2" >>/dev/null 2>&1
     ret=$?
     if [ $ret != 0 ]; then
         echo "python package:$1 not install"
-        return 1
+        return $ret_failed
     fi
-    return 0
+    return $ret_ok
+}
+
+function get_msame_file()
+{
+    get_arch=`arch`
+    if [[ $get_arch =~ "x86_64" ]];then
+        echo "arch x86_64"
+        local convert_url="https://aisbench.obs.myhuaweicloud.com/packet/msame/x86/msame"
+        wget $convert_url -O $1 --no-check-certificate
+    elif [[ $get_arch =~ "aarch64" ]];then
+        echo "arch arm64"
+        local convert_url="https://aisbench.obs.myhuaweicloud.com/packet/msame/arm/msame"
+        wget $convert_url -O $1 --no-check-certificate
+    else
+        echo "unknown!!"l
+    fi
 }

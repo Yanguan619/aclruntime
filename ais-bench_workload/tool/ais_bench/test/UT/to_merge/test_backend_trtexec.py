300e0b6e76e3a824d8f53fb67facd888b4715bf1
yanhe13 <yanhe13@huawei.com>
2024-07-16 20:22:19 +0800

    trtexec dt finish

--- ais-bench_workload/tool/ais_bench/test/UT/to_merge/test_backend_trtexec.py
+++ ais-bench_workload/tool/ais_bench/test/UT/to_merge/test_backend_trtexec.py
@@ -70,12 +70,49 @@ class TestClass:
         with pytest.raises(Exception):
             self.backend_trtexec.load(not_exist_model)
 
-    def test_parse_log(self):
-        assert self.backend_trtexec.parse_log("Throughput: 100.1\n") == 100.1
-        self.backend_trtexec.parse_log("H2D Latency= 1 H2D Latency= 1 H2D Latency= 1 H2D Latency= 1 H2D Latency= 1\n")
-        self.backend_trtexec.parse_log("GPU Compute Time= 1 GPU Compute Time= 1 GPU Compute Time= 1 GPU Compute Time= 1 GPU Compute Time= 1")
-        self.backend_trtexec.parse_log("D2H Latency= 1 D2H Latency= 1 D2H Latency= 1 D2H Latency= 1 D2H Latency= 1\n")
-        self.backend_trtexec.parse_log("Total Host Walltime: 100\n")
+    def test_parse_log_throughput(self):
+        perf = self.backend_trtexec.parse_log("Throughput: 120.699 qps\n")
+        assert abs(perf.throughput - 120.699) <= 1e-3
+
+    def test_parse_log_h2d_latency(self):
+        perf = self.backend_trtexec.parse_log(
+            "H2D Latency: min = 1.55066 ms, max = 1.57336 ms, mean = 1.55492 ms, " +
+            "median = 1.55444 ms, percentile(90%) = 1.55664 ms, " +
+            "percentile(95%) = 1.55835 ms, percentile(99%) = 1.56458 ms\n"
+        )
+        assert abs(perf.h2d_latency.min - 1.55066) <= 1e-5
+        assert abs(perf.h2d_latency.max - 1.57336) <= 1e-5
+        assert abs(perf.h2d_latency.mean - 1.55492) <= 1e-5
+        assert abs(perf.h2d_latency.median - 1.55444) <= 1e-5
+        assert abs(perf.h2d_latency.percentile - 1.55664) <= 1e-5
+
+    def test_parse_log_compute_time(self):
+        perf = self.backend_trtexec.parse_log(
+            "GPU Compute Time: min = 7.54407 ms, max = 10.1723 ms, " +
+            "mean = 8.23978 ms, median = 8.19409 ms, percentile(90%) = 8.5354 ms, " +
+            "percentile(95%) = 8.59131 ms, percentile(99%) = 9.90002 ms"
+        )
+        assert abs(perf.compute_time.min - 7.54407) <= 1e-5
+        assert abs(perf.compute_time.max - 10.1723) <= 1e-5
+        assert abs(perf.compute_time.mean - 8.23978) <= 1e-5
+        assert abs(perf.compute_time.median - 8.19409) <= 1e-5
+        assert abs(perf.compute_time.percentile - 8.5354) <= 1e-5
+
+    def test_parse_log_d2h_latency(self):
+        perf = self.backend_trtexec.parse_log(
+            "D2H Latency: min = 0.0130615 ms, max = 0.0170898 ms, mean = 0.015342 ms, " +
+            "median = 0.0153809 ms, percentile(90%) = 0.0162354 ms, " +
+            "percentile(95%) = 0.0163574 ms, percentile(99%) = 0.0168457 ms\n"
+        )
+        assert abs(perf.d2h_latency.min - 0.0130615) <= 1e-5
+        assert abs(perf.d2h_latency.max - 0.0170898) <= 1e-5
+        assert abs(perf.d2h_latency.mean - 0.015342) <= 1e-5
+        assert abs(perf.d2h_latency.median - 0.0153809) <= 1e-5
+        assert abs(perf.d2h_latency.percentile - 0.0162354) <= 1e-5
+
+    def test_parse_log_d2h_latency(self):
+        perf = self.backend_trtexec.parse_log("Total Host Walltime: 3.02405 s\n")
+        assert abs(perf.host_wall_time - 3.02405) <= 1e-5
 
     def test_warm_up(self):
         self.backend_trtexec.warm_up([], 100)
@@ -90,7 +127,7 @@ class TestClass:
         self.backend_trtexec.get_perf()
 
     def test_run(self):
-        self.backend_trtexec.run()
+        assert self.backend_trtexec.run() == []
 
 
 if __name__ == "__main__":

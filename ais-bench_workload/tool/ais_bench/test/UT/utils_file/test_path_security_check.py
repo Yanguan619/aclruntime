6785da2701b38751d038664965228d4df938fb7e
yanhe13 <yanhe13@huawei.com>
2024-07-26 13:04:57 +0800

    test path_security_check

--- ais-bench_workload/tool/ais_bench/test/UT/utils_file/test_path_security_check.py
+++ ais-bench_workload/tool/ais_bench/test/UT/utils_file/test_path_security_check.py
@@ -234,12 +234,20 @@ class TestClass:
 
     def test_ms_open_write(self, monkeypatch):
         monkeypatch.setattr("os.fdopen", lambda *arg, **kwargs: self.end_label)
+        monkeypatch.setattr("hasattr", lambda *arg: False)
+        with pytest.raises(Exception) as e:
+            ms_open(self.standard_file_path, mode="w")
+            if not "file owner is inconsistent" in str(e):
+                pytest.fail(f"Do not catch expected err! Actual error is {str(e)}")
+        monkeypatch.undo()
+
+        monkeypatch.setattr("os.fdopen", lambda *arg, **kwargs: self.end_label)
         monkeypatch.setattr("os.remove", lambda *arg: None)
         assert ms_open(self.standard_file_path, mode="w") ==self.end_label
 
     def test_ms_open_add(self, monkeypatch):
         monkeypatch.setattr("os.fdopen", lambda *arg, **kwargs: self.end_label)
-        monkeypatch.setattr("ais_bench.infer.common.path_security_check.FileStat.is_owner", lambda *arg: False)
+        monkeypatch.setattr("hasattr", lambda *arg: False)
         with pytest.raises(Exception) as e:
             ms_open(self.standard_file_path, mode="a")
             if not "file owner is inconsistent" in str(e):

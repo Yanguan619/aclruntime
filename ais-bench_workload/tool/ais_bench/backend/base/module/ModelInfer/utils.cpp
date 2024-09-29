eb9e683493c91498b11225de1176f95f3e627b5c
yanhe13 <yanhe13@huawei.com>
2024-09-29 08:15:09 +0800

    Utils::str2num

--- ais-bench_workload/tool/ais_bench/backend/base/module/ModelInfer/utils.cpp
+++ ais-bench_workload/tool/ais_bench/backend/base/module/ModelInfer/utils.cpp
@@ -67,6 +67,12 @@ int Utils::str2num(char* str)
     int flag = 0;
     const int decimal = 10;
     while (*str >= '0' && *str <= '9') {
+        if (n > INT_MAX / 10 || (n == INT_MAX / 10 && (*str - '0') > INT_MAX % 10)) {
+            throw std::runtime_error("string format int overflowed");
+        }
+        if (n < INT_MIN / 10 || (n == INT_MIN / 10 && (*str - '0') > -(INT_MIN % 10))) {
+            throw std::runtime_error("string format int underflowed");
+        }
         n = n * decimal + (*str - '0');
         str++;
     }

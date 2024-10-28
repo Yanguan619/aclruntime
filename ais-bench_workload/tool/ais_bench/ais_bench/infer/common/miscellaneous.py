efaa93ebb127fd8e8224b258cccc71a2a7aefca8
yanhe13 <yanhe13@huawei.com>
2024-10-28 19:13:03 +0800

    fix output file check

--- ais-bench_workload/tool/ais_bench/ais_bench/infer/common/miscellaneous.py
+++ ais-bench_workload/tool/ais_bench/ais_bench/infer/common/miscellaneous.py
@@ -93,6 +93,8 @@ def check_dump_path(dump_path):
         file_stat = FileStat(dump_path)
     except Exception as err:
         raise ValueError(f"weight path:{dump_path} is illegal. Please check.") from err
+    if not file_stat.is_dir:
+        raise TypeError(f"output path is not a directory")
     if not (file_stat.is_basically_legal(FILE_PERM_CHOICE.WRITE) and file_stat.is_basically_legal(FILE_PERM_CHOICE.READ)):
         raise ValueError(f"output path:{dump_path} is illegal. Please check.")
 

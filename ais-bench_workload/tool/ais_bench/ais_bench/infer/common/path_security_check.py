442aa27d59943e817c154cc782b634daea6401e4
yanhe13 <yanhe13@huawei.com>
2024-11-04 15:36:23 +0800

    interface path check fix

--- ais-bench_workload/tool/ais_bench/ais_bench/infer/common/path_security_check.py
+++ ais-bench_workload/tool/ais_bench/ais_bench/infer/common/path_security_check.py
@@ -335,9 +335,9 @@ def check_path_legality(path, perm=FILE_PERM_CHOICE.WRITE, max_size=MAX_SIZE_LIM
         else:
             raise ValueError(f"The path:{path} is not a directory.") # check path string content when init FileStat
     if not file_stat.is_basically_legal(perm):
-        raise ValueError(f"The path:{path} is illegal. Please check.")
-    if file_stat.is_file and not file_stat.is_legal_file_type(["config"]):
-        raise ValueError(f"The path:{path} is illegal. Please check.")
+        raise ValueError(f"The path:{path} is illegal. Please check the error log for more detail.")
+    if file_stat.is_file and not file_stat.is_legal_file_type(suffix):
+        raise ValueError(f"The suffix of path:{path} not in {suffix}. Please check.")
     if file_stat.is_file and not file_stat.is_legal_file_size(max_size):
         raise ValueError(f"The file:{path} size is larger than {max_size}. Please check.")
 

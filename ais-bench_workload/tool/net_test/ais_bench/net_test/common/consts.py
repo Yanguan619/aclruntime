7c8194583718a57669f22c5676833656976ae596
yanhe13 <yanhe13@huawei.com>
2024-11-15 14:44:11 +0800

    bugfix

--- ais-bench_workload/tool/net_test/ais_bench/net_test/common/consts.py
+++ ais-bench_workload/tool/net_test/ais_bench/net_test/common/consts.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 class PACKAGE_INFO:
-    version = "0.0.3"
+    version = "1.0.0"
 
 
 class SUB_MODULE_NAME:

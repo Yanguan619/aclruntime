be946056d101998e7b66325305edb2ab58a5ec8f
yanhe13 <yanhe13@huawei.com>
2024-11-20 12:07:59 +0800

    add test ssh

--- ais-bench_workload/tool/net_test/tests/src/net_test/main_function/test_ssh_operation.py
+++ ais-bench_workload/tool/net_test/tests/src/net_test/main_function/test_ssh_operation.py
@@ -24,11 +24,12 @@ class TestCheckFuncUtils(unittest.TestCase):
         with self.assertRaisesRegex(FileExistsError, "ssh_key_path not offered"):
             ssh_client_connect(ssh_client, node_info, "")
 
+    @patch("paramiko.ChannelFile", "1")
     @patch("paramiko.SSHClient.close")
     @patch("paramiko.ChannelFile.read")
     @patch("ais_bench.net_test.ssh.ssh_operation.ssh_client_connect")
     @patch("paramiko.SSHClient.exec_command")
-    def test_remote_exec_file_check(self, mock_exec, mock_connect, mock_read, mock_close):
+    def test_remote_exec_file_check(self, mock_exec, mock_connect, mock_read, mock_close, mock_channel):
         node_info = NodeInfo("XX", 1, "A", 123)
         stdout = paramiko.ChannelFile(["1"])
         stderr = paramiko.ChannelFile(["1"])
@@ -65,13 +66,12 @@ class TestCheckFuncUtils(unittest.TestCase):
         with self.assertRaisesRegex(RuntimeError, "failed, error log from node:"):
             remote_exec("./", node_info, "./")
 
-    @patch("paramiko.ChannelFile", "1")
     @patch("paramiko.SSHClient.close")
     @patch("scp.SCPClient.close")
     @patch("scp.SCPClient.put")
     @patch("scp.SCPClient")
     @patch("ais_bench.net_test.ssh.ssh_operation.ssh_client_connect")
-    def test_remote_put(self, mock_connect, mock_scp, mock_scp_put, mock_scp_close, mock_ssh_close, mock_channel):
+    def test_remote_put(self, mock_connect, mock_scp, mock_scp_put, mock_scp_close, mock_ssh_close):
         node_info = NodeInfo("XX", 1, "A", 123)
         mock_scp.side_effect = Exception('An error occurred')
         with self.assertRaisesRegex(RuntimeError, "open trans_client failed"):

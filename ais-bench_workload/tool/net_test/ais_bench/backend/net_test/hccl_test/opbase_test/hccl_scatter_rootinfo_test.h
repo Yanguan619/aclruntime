9bd609088477abb648f27f8926714cbab7d1407b
yanhe13 <yanhe13@huawei.com>
2025-01-06 20:48:40 +0800

    comment fix

--- ais-bench_workload/tool/net_test/ais_bench/backend/net_test/hccl_test/opbase_test/hccl_scatter_rootinfo_test.h
+++ ais-bench_workload/tool/net_test/ais_bench/backend/net_test/hccl_test/opbase_test/hccl_scatter_rootinfo_test.h
@@ -34,12 +34,12 @@ public:
     HcclOpBaseScatterTest();
     virtual ~HcclOpBaseScatterTest();
 
-    virtual int hccl_op_base_test(); //主函数
+    virtual int hccl_op_base_test(); // 主函数
 private:
     virtual int init_buf_val();  //（初始化host_buf，初始化check_buf，拷贝到send_buf） 其中需要调用HcclHostBufInit
-    virtual int check_buf_result();//（recv_buf拷贝到recvbufftemp,并且校验正确性）需要调用check_buf_init，校验正确性要调用CheckBufResultFloat
-    void cal_execution_time(float time);//统计耗时
-    virtual int destory_check_buf();//集合通信销毁
+    virtual int check_buf_result(); //（recv_buf拷贝到recvbufftemp,并且校验正确性）需要调用check_buf_init，校验正确性要调用CheckBufResultFloat
+    void cal_execution_time(float time); // 统计耗时
+    virtual int destory_check_buf(); // 集合通信销毁
 };
 }
 #endif

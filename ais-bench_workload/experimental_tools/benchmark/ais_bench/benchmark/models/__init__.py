8355bc3ae5373603d15302c348da933922182f85
yanhe13 <yanhe13@huawei.com>
2025-01-21 20:47:55 +0800

    change name to ais_bench/benchmark

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/__init__.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/__init__.py
@@ -1,5 +1,4 @@
 from .base import BaseModel, LMTemplateParser  # noqa: F401
 from .base_api import APITemplateParser, BaseAPIModel  # noqa: F401
 from .openai_api import OpenAI  # noqa: F401
-from .openai_api import OpenAISDK  # noqa: F401
 from .vllm import VLLM  # noqa: F401

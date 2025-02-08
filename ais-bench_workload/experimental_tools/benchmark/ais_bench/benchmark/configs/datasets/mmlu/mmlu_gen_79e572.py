85758d1bcebc8fe736eab686380c43625e307597
yanhe13 <yanhe13@huawei.com>
2025-02-08 15:02:30 +0800

    max_out_len 1

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/configs/datasets/mmlu/mmlu_gen_79e572.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/configs/datasets/mmlu/mmlu_gen_79e572.py
@@ -88,7 +88,7 @@ for _name in mmlu_all_sets:
             ice_token='</E>',
         ),
         retriever=dict(type=FixKRetriever, fix_id_list=[0, 1, 2, 3, 4]),
-        inferencer=dict(type=GenInferencer, max_out_len=1),
+        inferencer=dict(type=GenInferencer, max_out_len=1, batch_size=64),
     )
 
     mmlu_eval_cfg = dict(

47243c3560d5abfe8e28d9d0497a353fc589be24
yanhe13 <yanhe13@huawei.com>
2025-02-21 09:43:29 +0800

    add gpqa

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
@@ -2,3 +2,4 @@ from .gsm8k import * # noqa: F401, F403
 from .ceval import * # noqa: F401, F403
 from .boolq import * # noqa: F401, F403
 from .mmlu import * # noqa: F401, F403
+from .gpqa import * # noqa: F401, F403
---
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_api_vllm_general_gpqa.py
@@ -0,0 +1,39 @@
+from mmengine.config import read_base
+from ais_bench.benchmark.models import VLLMCustomAPI
+from ais_bench.benchmark.partitioners import NaivePartitioner
+from ais_bench.benchmark.runners.local_api import LocalAPIRunner
+from ais_bench.benchmark.tasks import OpenICLInferTask
+
+with read_base():
+    # from ais_bench.benchmark.configs.datasets.collections.chat_medium import datasets
+    from ais_bench.benchmark.configs.summarizers.medium import summarizer
+    from ais_bench.benchmark.configs.datasets.gpqa.gpqa_gen import gpqa_datasets
+
+datasets = [
+    *gpqa_datasets,
+]
+
+
+models = [
+    dict(
+        type=VLLMCustomAPI,
+        abbr='vllm-api-general',
+        max_seq_len = 4096,
+        query_per_second = 1,
+        rpm_verbose = False,
+        retry = 2,
+        host_ip = "localhost",
+        host_port = 8080,
+        enable_ssl = False,
+    )
+]
+
+
+infer = dict(partitioner=dict(type=NaivePartitioner),
+             runner=dict(
+                 type=LocalAPIRunner,
+                 max_num_workers=2,
+                 concurrent_users=2,
+                 task=dict(type=OpenICLInferTask)), )
+
+work_dir = 'outputs/api_vllm_general/'

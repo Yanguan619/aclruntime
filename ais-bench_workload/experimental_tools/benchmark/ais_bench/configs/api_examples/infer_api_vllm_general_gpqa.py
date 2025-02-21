309518b80953694fddb24df70fc3ab3e581071e2
yanhe13 <yanhe13@huawei.com>
2025-02-21 09:47:47 +0800

    sample fix

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_api_vllm_general_gpqa.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_api_vllm_general_gpqa.py
@@ -25,6 +25,13 @@ models = [
         host_ip = "localhost",
         host_port = 8080,
         enable_ssl = False,
+        generation_kwargs = dict(
+            temperature = 0.5,
+            top_k = 10,
+            top_p = 0.95,
+            seed = None,
+            repetition_penalty = 1.03,
+        ),
     )
 ]
 

2cf0a264a13d729d63b3e468b82cd6e0aafa7acc
yanhe13 <yanhe13@huawei.com>
2025-02-26 15:07:37 +0800

    delete gpqa doc

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/utils/datasets.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/utils/datasets.py
@@ -6,82 +6,6 @@ import urllib.request
 
 from .logging import get_logger
 
-DATASETS_URL_CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../ais_bench/datasets/datasets_urls.json")
-
-
-class DatasetDownLoader:
-    def __init__(self, dataset_path):
-        self.dataset_path = os.path.abspath(dataset_path)
-        with open(DATASETS_URL_CONFIG_PATH, "r") as file:
-            self.urls_data = json.load(file)
-
-    def __call__(self):
-        self.auto_download_dataset()
-
-    def get_dataset_info_from_path(self):
-        path_list = self.dataset_path.split("/") # only linux
-        for idx, basename in enumerate(reversed(path_list)):
-            if self.urls_data.get(basename):
-                save_path = "/" + "/".join(path_list[:idx - 1])
-                # need to check save_path
-                return save_path, basename
-        raise ValueError(f"Auto download datasets path is illegal!")
-
-    def calculate_sha256(self, file_path):
-        sha256_hash = hashlib.sha256()
-        try:
-            with open(file_path, 'rb') as file:
-                for chunk in iter(lambda: file.read(4096), b""):
-                    sha256_hash.update(chunk)
-            return sha256_hash.hexdigest()
-        except FileNotFoundError:
-            return None
-
-    def verify_sha256(self, file_path, expected_sha256):
-        actual_sha256 = self.calculate_sha256(file_path)
-        if expected_sha256 is not None:
-            if actual_sha256 == expected_sha256:
-                return
-            else:
-                raise ValueError("Sha-256 hash info check failed!")
-
-    def auto_download_dataset(self):
-        # customized dataset path
-        if os.path.exists(self.dataset_path):
-            return
-
-        save_path, dataset_name = self.get_dataset_info_from_path()
-        dataset_dir_path = os.path.join(save_path, dataset_name)
-
-        if not os.path.exists(save_path):
-            os.makedirs(save_path) # need to replace by a safe func
-
-        if os.path.exists(dataset_dir_path):
-            get_logger().warning(f"Dataset: {dataset_name} is exist, won't auto download")
-            return
-
-        # download dataset zip
-        dataset_zip_path = dataset_dir_path + ".zip"
-        if not os.path.exists(dataset_zip_path):
-            url = self.urls_data.get(dataset_name, None).get("url", None)
-            if url is None:
-                raise ValueError(f"Url of datasets: {dataset_name} is None, " + \
-                                "please check the content of {DATASETS_URL_CONFIG_PATH}")
-            try:
-                urllib.request.urlretrieve(url, dataset_zip_path)
-            except Exception as err:
-                raise RuntimeError(f"Auto download dataset: {dataset_name} failed!") from err
-            get_logger().info(f"Auto download dataset: {dataset_name} success")
-
-        # check hash info
-        hash_info = self.urls_data.get(dataset_name, None).get("hash_info", None)
-        self.verify_sha256(dataset_zip_path, hash_info)
-
-        with zipfile.ZipFile(dataset_zip_path, 'r') as zip_ref:
-            zip_ref.extractall(self.dataset_path)
-
-        os.remove(dataset_zip_path)
-
 
 def get_cache_dir(default_dir):
     return os.environ.get('AIS_BENCH_DATASETS_CACHE', default_dir)
@@ -107,10 +31,6 @@ def get_data_path(dataset_path: str, local_mode: bool = True):
     # For relative path, with CACHE_DIR
     if local_mode:
         local_path = os.path.join(cache_dir, dataset_path)
-
-        # auto download
-        downloader = DatasetDownLoader(local_path)
-        downloader()
 
         if not os.path.exists(local_path):
             raise FileExistsError(f'Dataset path: {local_path} is not exist!')

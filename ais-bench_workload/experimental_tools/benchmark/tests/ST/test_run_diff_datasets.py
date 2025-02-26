26b6b2e674524b68e9aa86d6102f7bac595ab11a
yanhe13 <yanhe13@huawei.com>
2025-02-26 11:49:18 +0800

    add gpqa doc

--- ais-bench_workload/experimental_tools/benchmark/tests/ST/test_run_diff_datasets.py
+++ ais-bench_workload/experimental_tools/benchmark/tests/ST/test_run_diff_datasets.py
@@ -173,4 +173,36 @@ class TestClass:
         vis_md_path = os.path.join(self.test_data_path, f"{fake_time_str}/summary/summary_{fake_time_str}.md")
         assert os.path.exists(vis_md_path)
 
+    def test_vllm_api_all_qwen2_7b_aime2024(self, monkeypatch):
+        fake_prediction = "A"
+        fake_time_str = "fake_time_gpqa"
+        monkeypatch.setattr('sys.argv',
+            ["ais_bench", "--models", "vllm_api_general", "--datasets", "gpqa_gen",
+            "--mode", "all", "-w", self.test_data_path])
+        monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api.VLLMCustomAPI._get_service_model_path", lambda *arg: "qwen2")
+        monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api.VLLMCustomAPI._generate", lambda *arg: fake_prediction)
+        monkeypatch.setattr("ais_bench.benchmark.cli.main.get_current_time_str", lambda *arg: fake_time_str)
+        main()
+
+        # check infer out
+        infer_outputs_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/predictions/vllm-api-general/GPQA_diamond.json")
+        assert os.path.exists(infer_outputs_json_path)
+        with open(infer_outputs_json_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        assert data.get(f"0").get("prediction") == fake_prediction
+
+        # check eval out
+        results_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/results/vllm-api-general/GPQA_diamond.json")
+        with open(results_json_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        assert data.get("accuracy") is not None
+
+        # check vis
+        vis_csv_path = os.path.join(self.test_data_path, f"{fake_time_str}/summary/summary_{fake_time_str}.csv")
+        assert os.path.exists(vis_csv_path)
+        vis_txt_path = os.path.join(self.test_data_path, f"{fake_time_str}/summary/summary_{fake_time_str}.txt")
+        assert os.path.exists(vis_txt_path)
+        vis_md_path = os.path.join(self.test_data_path, f"{fake_time_str}/summary/summary_{fake_time_str}.md")
+        assert os.path.exists(vis_md_path)
+
 

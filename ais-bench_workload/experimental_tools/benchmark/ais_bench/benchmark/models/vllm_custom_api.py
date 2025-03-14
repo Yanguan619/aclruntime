12b29d36f62d77f456fccc847c2ab23e3b1dc766
zjks98 <1778481322@qq.com>
2025-03-14 16:01:52 +0800

    fix log bug

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api.py
@@ -269,7 +269,7 @@ class VLLMCustomAPIOld(BaseAPIModel):
                 continue
 
             if response.get('text') is None:
-                raise RuntimeError("Get unexpected response: {response} from service!")
+                raise RuntimeError(f"Get unexpected response: {response} from service!")
             return response['text'][0]
 
         raise RuntimeError('Calling VLLM failed after retrying for '

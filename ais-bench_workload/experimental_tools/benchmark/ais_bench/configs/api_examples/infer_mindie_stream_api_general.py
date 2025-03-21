2e80ed4a262dda1d7ab4f446f7ac676f39d1fc8e
yanhe13 <yanhe13@huawei.com>
2025-03-21 14:52:08 +0800

    custom config fix

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_mindie_stream_api_general.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_mindie_stream_api_general.py
@@ -21,10 +21,11 @@ models = [
         query_per_second = 1,
         rpm_verbose = False,
         retry = 2,
-        host_ip = "localhost",
-        host_port = 8080,
+        host_ip = "localhost", # 推理服务的IP
+        host_port = 8080, # 推理服务的端口
         enable_ssl = False,
-        generation_kwargs = dict(
+        max_out_len = 512, # 最大输出tokens长度
+        generation_kwargs = dict( # 后处理参数参考https://www.hiascend.com/document/detail/zh/mindie/100/mindieservice/servicedev/mindie_service0090.html 中的parameters的子参数
             temperature = 0.5,
             top_k = 10,
             top_p = 0.95,
@@ -49,4 +50,4 @@ infer = dict(partitioner=dict(type=NaivePartitioner),
                  concurrent_users=2,
                  task=dict(type=OpenICLInferTask)), )
 
-work_dir = 'outputs/api-mindie-stream/'
+work_dir = 'outputs/api-mindie-stream/' # 工作路径
--- ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_vllm_api_old.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/configs/api_examples/infer_vllm_api_old.py
@@ -5,15 +5,12 @@ from ais_bench.benchmark.runners.local_api import LocalAPIRunner
 from ais_bench.benchmark.tasks import OpenICLInferTask
 
 with read_base():
-    # from ais_bench.benchmark.configs.datasets.collections.chat_medium import datasets
     from ais_bench.benchmark.configs.summarizers.example import summarizer
     from all_dataset_configs import *
 
 datasets = [
     *gsm8k_0_shot_cot_str,
 ]
-
-
 
 models = [
     dict(
@@ -23,10 +20,11 @@ models = [
         query_per_second = 1,
         rpm_verbose = False,
         retry = 2,
-        host_ip = "localhost",
-        host_port = 8080,
+        host_ip = "localhost", # 推理服务的IP
+        host_port = 8080, # 推理服务的端口
         enable_ssl = False,
-        generation_kwargs = dict(
+        max_out_len = 512, # 最大输出tokens长度
+        generation_kwargs = dict( # 后处理参数参考https://www.hiascend.com/document/detail/zh/mindie/100/mindieservice/servicedev/mindie_service0072.html中的请求参数
             temperature = 0.5,
             top_k = 10,
             top_p = 0.95,

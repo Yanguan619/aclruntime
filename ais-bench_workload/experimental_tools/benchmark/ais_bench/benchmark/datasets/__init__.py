aed57e7601e2e521f0be213974eca8756d9e7a8d
yanhe13 <yanhe13@huawei.com>
2025-03-26 09:50:38 +0800

    add piqa

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
@@ -11,3 +11,4 @@ from .mmlu_pro import * # noqa: F401, F403
 from .humaneval import * # noqa: F401, F403
 from .custom import *
 from .livecodebench import * # noqa: F401, F403
+from .piqa import *  # noqa: F401, F403

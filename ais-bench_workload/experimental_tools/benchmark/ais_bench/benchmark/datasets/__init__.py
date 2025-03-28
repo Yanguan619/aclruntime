c813f1196280660d92ef470216b9c57c6602883f
yanhe13 <yanhe13@huawei.com>
2025-03-28 09:10:34 +0800

    add triviaqa

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
@@ -17,3 +17,4 @@ from .winogrande import * # noqa: F401, F403
 from .mbpp import * # noqa: F401，F403
 from .agieval import * # noqa: F401, F403
 from .hellaswag import * # noqa: F401, F403
+from .triviaqa import * # noqa: F401, F403

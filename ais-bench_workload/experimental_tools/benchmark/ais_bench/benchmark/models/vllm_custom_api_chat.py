f47caee358320156b2d757cccfceed8f23a0c99f
yanhe13 <yanhe13@huawei.com>
2025-03-29 15:47:23 +0800

    debug

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
@@ -298,6 +298,7 @@ class VLLMCustomAPIChatStream(BaseAPIModel):
                 url = os.path.join(self.base_url, "chat/completions")
                 raw_response = requests.post(url, headers=header, data=json.dumps(data), stream=True)
                 for res_ in self.process_response(raw_response):
+                    self.logger.info(f"res_: {res_}")
                     if self.res_key not in res_:
                         continue
                     response.append(res_[self.res_key])

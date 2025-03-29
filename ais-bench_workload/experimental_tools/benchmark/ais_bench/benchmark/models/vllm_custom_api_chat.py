b7492c38f4134bd169103f6772a5d4ddb0fd2098
yanhe13 <yanhe13@huawei.com>
2025-03-29 16:02:52 +0800

    debug

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
@@ -297,7 +297,6 @@ class VLLMCustomAPIChatStream(BaseAPIModel):
                 url = os.path.join(self.base_url, "chat/completions")
                 raw_response = requests.post(url, headers=header, data=json.dumps(data), stream=True)
                 for res_ in self.process_response(raw_response):
-                    self.logger.info(f"res_: {res_}")
                     if res_.get("choices" ) is None:
                         continue
                     else:

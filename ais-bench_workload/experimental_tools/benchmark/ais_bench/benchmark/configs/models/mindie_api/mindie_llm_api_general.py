fac59428e7c5af919d3e753488de8259292769c9
zjks98 <1778481322@qq.com>
2025-04-06 13:53:58 +0800

    fix Variable description

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/configs/models/mindie_api/mindie_llm_api_general.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/configs/models/mindie_api/mindie_llm_api_general.py
@@ -8,11 +8,11 @@ models = [
 
         world_size = 2,  # 本次推理使用的卡总数
 
-        block_size = 128,  # block size是warm up时进行预先计算内存所需的参数
+        block_size = 128,  # 初始化推理对象所需参数，预先计算内存所需的参数
         model_name = "qwen",  # 模型名称
         data_type = "bf16",  # 模型配置数据类型
         weight_dir = "/data1/qwen2-7b-instruct-cp",  # 模型权重路径
-        max_position_embedding = 5120,  # 模型预先分配内存所需的参数
+        max_position_embedding = -1,  # 初始化推理对象所需参数，-1表示使用input_length + output_length
         is_chat_model = False,  # 是否使用chat模板
         decode_batch_size = 32,  # decode阶段的batchsize，与数据集推理时设定的batchsize相同
         prefill_batch_size = 0,  # prefill阶段的batchsize
@@ -26,9 +26,9 @@ models = [
         moe_ep = -1,
         trust_remote_code = False,  # 是否信任远端代码
         ignore_eos = False,  # 是否忽略推理终止符
-        input_length = 4096,  # warm_up参数，input长度
-        output_length = 1024,  # warm_up参数，output长度
-        run_cfg = dict(num_gpus=2, num_procs=2),  # 多卡/多机多卡 参数，NPU侧使用torchrun拉起任务
+        input_length = 4096,  # 初始化推理对象参数，input长度
+        output_length = 1024,  # 初始化推理对象参数，output长度
+        run_cfg = dict(num_gpus=2, num_procs=2),  # 多卡/多机多卡 参数，使用torchrun拉起任务
 
         atb_layer_internal_tensor_reuse = "1", # modeltest创建模型实例时默认设定的环境变量
         atb_operation_execute_async = "1",
@@ -37,6 +37,5 @@ models = [
         atb_workspace_mem_alloc_global = "1",
         atb_context_workspace_size = "0",
         atb_launch_kernel_with_tiling = "1",
-
     )
 ]

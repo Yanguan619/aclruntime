49e28c6d07e3b19aab9663622ef0e98c14080422
yanhe13 <yanhe13@huawei.com>
2025-04-10 09:53:56 +0800

    add st

--- ais-bench_workload/experimental_tools/benchmark/tests/ST/test_mindie_llm_api.py
+++ ais-bench_workload/experimental_tools/benchmark/tests/ST/test_mindie_llm_api.py
@@ -105,14 +105,14 @@ class TestClass:
         main()
 
         # check perf standard out
-        perf_data_path = os.path.join(self.test_data_path, f"{fake_time_str}/performance/mindie-llm-api/{datasets_abbr_name}.json")
+        perf_data_path = os.path.join(self.test_data_path, f"{fake_time_str}/performances/mindie-llm-api/{datasets_abbr_name}.json")
         assert os.path.exists(perf_data_path)
         with open(perf_data_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         assert data.get("e2e_throughput") is not None
 
         # check perf special out
-        perf_sp_data_path = os.path.join(self.test_data_path, f"{fake_time_str}/results/mindie-llm-api/pa_runner_special_perf_data__{datasets_abbr_name}.json")
+        perf_sp_data_path = os.path.join(self.test_data_path, f"{fake_time_str}/performances/mindie-llm-api//pa_runner_special_perf_data__{datasets_abbr_name}.json")
         with open(perf_sp_data_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         for key in ["batch_size", "seq_len_in", "seq_len_out", "total_time", "first_token_time", "non_first_token_time", "e2e_time", "e2e_throughput"]:

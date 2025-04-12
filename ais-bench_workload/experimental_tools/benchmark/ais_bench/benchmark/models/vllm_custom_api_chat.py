0819b821911864ee1124e4e6a9689a7e271f3aa9
yanhe13 <yanhe13@huawei.com>
2025-04-12 08:50:02 +0800

    yanhe

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
@@ -86,7 +86,8 @@ class VLLMCustomAPIChat(BaseAPIModel):
         Returns:
             List[str]: A list of generated strings.
         """
-        batch_size = len(inputs)
+        batch_size = kwargs.get("batch_size", len(inputs))
+        self.logger.info("running generate thread")
         with ThreadPoolExecutor(max_workers=batch_size) as executor:
             results = list(
                 tqdm(executor.map(self._generate, inputs,

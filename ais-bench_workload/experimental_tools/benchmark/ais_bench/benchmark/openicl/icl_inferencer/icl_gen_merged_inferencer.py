9030e4c97d31789c22e0884ce28dfd7c2189a309
yanhe13 <yanhe13@huawei.com>
2025-04-12 09:44:11 +0800

    warning fix

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/openicl/icl_inferencer/icl_gen_merged_inferencer.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/openicl/icl_inferencer/icl_gen_merged_inferencer.py
@@ -173,6 +173,7 @@ class GenMergedInferencer(GenInferencer):
 
             end_round = math.ceil(total_ds_len / self.batch_size)
             start_round = math.floor((index + 1) / self.batch_size)
+            index = self.batch_size * start_round - 1
 
             for i in tqdm(range(start_round, end_round), desc="Batch Infer Processing", unit="batch", dynamic_ncols=True):
                 if total_ds_len % self.batch_size != 0 and i == end_round - 1:

0e9131485bc6e5fa893f6f1af51d45a2aa0e3d6d
yanhe13 <yanhe13@huawei.com>
2025-04-15 20:11:14 +0800

    perf summarizer

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/summarizers/default_perf.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/summarizers/default_perf.py
@@ -71,7 +71,7 @@ class DefaultPerfSummarizer:
 
         for model in self.model_abbrs:
             for dataset in self.dataset_abbrs:
-                perf_result_dir = osp.join(self.work_dir, "performances", model, dataset)
+                perf_result_dir = osp.join(self.work_dir, "performances", model)
                 if osp.exists(osp.join(perf_result_dir, f"{dataset}.csv")):
                     perf_tables[f"{model}/{dataset}"] = self._load_csv_to_table(osp.join(perf_result_dir, f"{dataset}.csv"))
                 elif osp.exists(osp.join(perf_result_dir, f"{dataset}.json")):

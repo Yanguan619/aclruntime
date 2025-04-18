8ef6c42f69ea8f7310e39f49d82bf115bb4c804a
yanhe13 <yanhe13@huawei.com>
2025-04-18 15:24:34 +0800

    TGI stream

---
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/configs/models/tgi_api/tgi_stream_api_general.py
@@ -0,0 +1,25 @@
+from ais_bench.benchmark.models import TGICustomAPIStream
+
+models = [
+    dict(
+        attr="service", # local or service
+        type=TGICustomAPIStream,
+        abbr='tgi-stream-api-general',
+        path="",
+        max_seq_len = 4096,
+        query_per_second = 1,
+        rpm_verbose = False,
+        retry = 2,
+        host_ip = "localhost", # 推理服务的IP
+        host_port = 8080, # 推理服务的端口
+        enable_ssl = False,
+        max_out_len = 512, # 最大输出tokens长度
+        generation_kwargs = dict( # 后处理参数参考huggingface.github.io/text-generation-inference/
+            temperature = 0.5,
+            top_k = 10,
+            top_p = 0.95,
+            do_sample = True,
+            repetition_penalty = 1.03,
+        )
+    )
+]

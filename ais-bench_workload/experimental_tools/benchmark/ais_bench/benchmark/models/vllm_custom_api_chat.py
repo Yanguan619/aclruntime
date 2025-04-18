dfb93d278a05a98a0c13ae8e624616f48ededad6
yanhe13 <yanhe13@huawei.com>
2025-04-18 10:04:09 +0800

    chat encode fix

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/models/vllm_custom_api_chat.py
@@ -6,7 +6,7 @@ import time
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 
 import httpx
 import jieba

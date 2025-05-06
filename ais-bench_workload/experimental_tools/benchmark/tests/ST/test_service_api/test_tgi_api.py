436f89043456664cbb00a75db01626e0e3c1b82d
yanhe13 <yanhe13@huawei.com>
2025-05-06 20:38:02 +0800

    tgi text st

--- ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_tgi_api.py
+++ ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_tgi_api.py
@@ -11,7 +11,7 @@ import pandas as pd
 class Response:
     def __init__(self):
         data = {"generated_text": "11"}
-        self.data = f"data: {json.dumps(data)}\n".encode()
+        self.data = f"{json.dumps(data)}".encode()
 
 
 class TestClass:

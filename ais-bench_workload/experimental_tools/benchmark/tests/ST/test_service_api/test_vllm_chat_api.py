111397bc2b6dc7cc44dbe64fbb17f121a2837b8d
yanhe13 <yanhe13@huawei.com>
2025-05-06 20:59:16 +0800

    text st

--- ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_vllm_chat_api.py
+++ ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_vllm_chat_api.py
@@ -38,22 +38,22 @@ class TestClass:
         datasets_abbr_name = "aime2024"
         datasets_script_name = "aime2024_gen_0_shot_str"
         monkeypatch.setattr('sys.argv',
-            ["ais_bench", "--models", "vllm_api_general", "--datasets", datasets_script_name,
+            ["ais_bench", "--models", "vllm_api_general_chat", "--datasets", datasets_script_name,
             "--mode", "all", "-w", self.test_data_path])
-        monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api.VLLMCustomAPI._get_service_model_path", lambda *arg: "qwen2")
+        monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api_chat.VLLMCustomAPIChat._get_service_model_path", lambda *arg: "qwen2")
         monkeypatch.setattr("urllib3.PoolManager.request", lambda *args, **kwargs: Response())
         monkeypatch.setattr("ais_bench.benchmark.cli.main.get_current_time_str", lambda *arg: fake_time_str)
         main()
 
         # check infer out
-        infer_outputs_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/predictions/vllm-api-general/{datasets_abbr_name}.json")
+        infer_outputs_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/predictions/vllm-api-general-chat/{datasets_abbr_name}.json")
         assert os.path.exists(infer_outputs_json_path)
         with open(infer_outputs_json_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         assert data.get(f"0").get("prediction") == fake_prediction
 
         # check eval out
-        results_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/results/vllm-api-general/{datasets_abbr_name}.json")
+        results_json_path = os.path.join(self.test_data_path, f"{fake_time_str}/results/vllm-api-general-chat/{datasets_abbr_name}.json")
         with open(results_json_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         assert data.get("accuracy") is not None

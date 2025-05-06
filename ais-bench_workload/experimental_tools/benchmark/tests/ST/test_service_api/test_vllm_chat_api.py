c0c28071ec69b50507d6eef0bb203211b5e80bc2
yanhe13 <yanhe13@huawei.com>
2025-05-06 20:55:03 +0800

    text st

--- ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_vllm_chat_api.py
+++ ais-bench_workload/experimental_tools/benchmark/tests/ST/test_service_api/test_vllm_chat_api.py
@@ -41,7 +41,7 @@ class TestClass:
             ["ais_bench", "--models", "vllm_api_general", "--datasets", datasets_script_name,
             "--mode", "all", "-w", self.test_data_path])
         monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api.VLLMCustomAPI._get_service_model_path", lambda *arg: "qwen2")
-        monkeypatch.setattr("ais_bench.benchmark.models.vllm_custom_api.VLLMCustomAPI._generate", lambda *arg: fake_prediction)
+        monkeypatch.setattr("urllib3.PoolManager.request", lambda *args, **kwargs: Response())
         monkeypatch.setattr("ais_bench.benchmark.cli.main.get_current_time_str", lambda *arg: fake_time_str)
         main()
 

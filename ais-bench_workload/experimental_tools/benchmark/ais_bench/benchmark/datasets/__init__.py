0b2a700cf43560eba6ee3ef254024b30e71c052e
zjks98 <1778481322@qq.com>
2025-05-12 17:01:01 +0800

    add lcsts/siqa/xsum datasets

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/datasets/__init__.py
@@ -26,3 +26,6 @@ from .humanevalx.humanevalx import * # noqa: F401, F403
 from .bbh import * # noqa: F401, F403
 from .race import *
 from .lambada import * # noqa: F401, F403
+from .lcsts import * # noqa: F401, F403
+from .siqa import * # noqa: F401, F403
+from .xsum import * # noqa: F401, F403

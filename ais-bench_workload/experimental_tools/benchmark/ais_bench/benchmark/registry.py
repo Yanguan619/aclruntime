facd470e531ceb53804e431d44f33519c90e93f1
yanhe13 <yanhe13@huawei.com>
2025-05-13 17:26:45 +0800

    register fix

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/registry.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/registry.py
@@ -5,6 +5,8 @@ from mmengine.registry import METRICS as MMENGINE_METRICS
 from mmengine.registry import Registry as OriginalRegistry
 from ais_bench.benchmark.global_consts import CUSTOM_PACKAGE_DIR
 
+
+
 def load_class(class_path):
     """动态加载类路径并返回类对象"""
     try:
@@ -18,6 +20,18 @@ def load_class(class_path):
         raise ValueError(f"无法加载类 {class_path}: {e}") from e
 
 
+def get_locations(module_dir):
+    locations = [f'ais_bench.benchmark.{module_dir}']
+    custom_loc = f'{CUSTOM_PACKAGE_DIR}.{module_dir}'
+    if CUSTOM_PACKAGE_DIR:
+        try:
+            _ = __import__(custom_loc, fromlist=[True])
+            locations.append(custom_loc)
+        except ImportError:
+            pass
+    return locations
+
+
 class Registry(OriginalRegistry):
 
     # override the default force behavior
@@ -29,35 +43,35 @@ class Registry(OriginalRegistry):
         return super().register_module(name, force, module)
 
 
-PARTITIONERS = Registry('partitioner', locations=['ais_bench.benchmark.partitioners', f'{CUSTOM_PACKAGE_DIR}.partitioners'])
-RUNNERS = Registry('runner', locations=['ais_bench.benchmark.runners', f'{CUSTOM_PACKAGE_DIR}.runners'])
-TASKS = Registry('task', locations=['ais_bench.benchmark.tasks', f'{CUSTOM_PACKAGE_DIR}.tasks'])
-MODELS = Registry('model', locations=['ais_bench.benchmark.models', f'{CUSTOM_PACKAGE_DIR}.models'])
+PARTITIONERS = Registry('partitioner', locations=get_locations('partitioners'))
+RUNNERS = Registry('runner', locations=get_locations('runners'))
+TASKS = Registry('task', locations=get_locations('tasks'))
+MODELS = Registry('model', locations=get_locations('models'))
 # TODO: LOAD_DATASET -> DATASETS
-LOAD_DATASET = Registry('load_dataset', locations=['ais_bench.benchmark.datasets', f'{CUSTOM_PACKAGE_DIR}.datasets'])
+LOAD_DATASET = Registry('load_dataset', locations=get_locations('datasets'))
 TEXT_POSTPROCESSORS = Registry(
-    'text_postprocessors', locations=['ais_bench.benchmark.utils.text_postprocessors', f'{CUSTOM_PACKAGE_DIR}.utils.text_postprocessors'])
+    'text_postprocessors', locations=get_locations('utils.text_postprocessors'))
 DICT_POSTPROCESSORS = Registry(
-    'dict_postprocessors', locations=['ais_bench.benchmark.utils.dict_postprocessors', f'{CUSTOM_PACKAGE_DIR}.utils.dict_postprocessors'])
+    'dict_postprocessors', locations=get_locations('utils.dict_postprocessors'))
 
-EVALUATORS = Registry('evaluators', locations=['ais_bench.benchmark.evaluators', f'{CUSTOM_PACKAGE_DIR}.evaluators'])
+EVALUATORS = Registry('evaluators', locations=get_locations('evaluators'))
 
 ICL_INFERENCERS = Registry('icl_inferencers',
-                           locations=['ais_bench.benchmark.openicl.icl_inferencer', f'{CUSTOM_PACKAGE_DIR}.openicl.icl_inferencer'])
+                           locations=get_locations('openicl.icl_inferencer'))
 ICL_RETRIEVERS = Registry('icl_retrievers',
-                          locations=['ais_bench.benchmark.openicl.icl_retriever', f'{CUSTOM_PACKAGE_DIR}.openicl.icl_retriever'])
+                          locations=get_locations('openicl.icl_retriever'))
 ICL_DATASET_READERS = Registry(
     'icl_dataset_readers',
-    locations=['ais_bench.benchmark.openicl.icl_dataset_reader', f'{CUSTOM_PACKAGE_DIR}.openicl.icl_dataset_reader'])
+    locations=get_locations('openicl.icl_dataset_reader'))
 ICL_PROMPT_TEMPLATES = Registry(
     'icl_prompt_templates',
-    locations=['ais_bench.benchmark.openicl.icl_prompt_template', f'{CUSTOM_PACKAGE_DIR}.openicl.icl_prompt_template'])
+    locations=get_locations('openicl.icl_prompt_template'))
 ICL_EVALUATORS = Registry('icl_evaluators',
-                          locations=['ais_bench.benchmark.openicl.icl_evaluator', f'{CUSTOM_PACKAGE_DIR}.openicl.icl_evaluator'])
+    locations=get_locations('openicl.icl_evaluator'))
 METRICS = Registry('metric',
                    parent=MMENGINE_METRICS,
-                   locations=['ais_bench.benchmark.metrics', f'{CUSTOM_PACKAGE_DIR}.metrics'])
-TOT_WRAPPER = Registry('tot_wrapper', locations=['ais_bench.benchmark.datasets', f'{CUSTOM_PACKAGE_DIR}.datasets'])
+                   locations=get_locations('metrics'))
+TOT_WRAPPER = Registry('tot_wrapper', locations=get_locations('datasets'))
 
 
 def build_from_cfg(cfg):

5e1c0e8306d3b76f7353e49ba46ac243fcbb5a00
卤辣鲜香 <love@zerli.cn>
2025-05-26 07:04:33 +0000

    【oec-ascend】aclnnadd单算子开发测试用例

---
+++ oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAdd/ascend_test_nn.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+import oec
+
+oec.TestCase(
+    group=("应用开发", "基础功能"),
+    name="ACLNN_ADD",
+    cmd="./run.sh",
+    expect=0,
+    exclude=["failed", "Failed", "FAILED", "error", "ERROR", "Error"],
+)

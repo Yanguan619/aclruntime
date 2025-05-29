e63ba0e813dbb8ed5842b03bea6a257151f252ba
卤辣鲜香 <love@zerli.cn>
2025-05-29 03:41:50 +0000

    【oec-ascend】更新ATC，AOE测试用例，清理运行残留

--- oec-ascend/oec/resource/ModelDev/ATCTool/ascend_test_atc.py
+++ oec-ascend/oec/resource/ModelDev/ATCTool/ascend_test_atc.py
@@ -3,21 +3,31 @@ class ATCTest(oec.TestCase):
     def execute_command(self):
         import acl
         soc = acl.get_soc_name()
-        self.execute_command_with_cmd(f"{self.get_cmd()} --soc_version={soc}")
+        self.execute_command_with_cmd(f'{self.get_cmd()} --soc_version="{soc}"')
 ATCTest(
     group= ("模型开发","模型编译"),
     name = "MODEL_ATC_SINGLE_ADD",
-    cmd="atc --singleop=add.json --output=out/op_model1 && rm -rf out/op_model1"
+    cmd="atc --singleop=add.json --output=out/op_model1"
     )
 
 ATCTest(
     group= ("模型开发","模型编译"),
     name = "MODEL_ATC_SINGLE_CONV2D",
-    cmd="atc --singleop=conv2d.json --output=out/op_model2 && rm -rf out/op_model2"
+    cmd="atc --singleop=conv2d.json --output=out/op_model2",
+    optional=True
     )
 
 ATCTest(
     group= ("模型开发","模型编译"),
     name = "MODEL_ATC_SINGLE_DYNAMIC_SHAPE",
-    cmd="atc --singleop=dynamic_shape.json --output=out/op_model3 && rm -rf out/op_model3"
+    cmd="atc --singleop=dynamic_shape.json --output=out/op_model3",
+    optional=True
+    )
+
+oec.TestCase(
+    group= ("模型开发","模型编译"),
+    name = "MODEL_ATC_CLEAN_OUTPUT",
+    cmd="rm -rf out && rm -rf fusion_result.json",
+    auxiliary=True,
+    optional=True
     )

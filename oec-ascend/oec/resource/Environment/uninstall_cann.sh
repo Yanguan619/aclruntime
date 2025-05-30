837e9be6e7252ded579d82599afa539d17e2707a
卤辣鲜香 <love@zerli.cn>
2025-05-30 06:32:22 +0000

    【oec-ascend】添加CANN 卸载用例

---
+++ oec-ascend/oec/resource/Environment/uninstall_cann.sh
@@ -0,0 +1,11 @@
+cd $1
+echo $1 $2
+count=$(find . -type f -name "$2*" | wc -l)
+if [ "$count" -ne 1 ]; then
+    echo numer of $2 is not equal to 1
+    exit 1
+fi
+echo ./$2* --uninstall --install-path=$(realpath $1)/Ascend
+chmod +x $2*
+./$2* --uninstall --install-path=$(realpath $1)/Ascend
+exit $?

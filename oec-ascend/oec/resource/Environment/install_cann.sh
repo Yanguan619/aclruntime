2590c51d2d587c7482b9ea81dce0958d08c6f152
卤辣鲜香 <love@zerli.cn>
2025-05-30 06:32:02 +0000

    【oec-ascend】CANN安装用例 日志信息优化

--- oec-ascend/oec/resource/Environment/install_cann.sh
+++ oec-ascend/oec/resource/Environment/install_cann.sh
@@ -2,7 +2,7 @@ cd $1
 echo $1 $2
 count=$(find . -type f -name "$2*" | wc -l)
 if [ "$count" -ne 1 ]; then
-    echo $2* is not equal to 1
+    echo numer of $2 is not equal to 1
     exit 1
 fi
 echo ./$2* --install --quiet --install-path=$(realpath $1)/Ascend

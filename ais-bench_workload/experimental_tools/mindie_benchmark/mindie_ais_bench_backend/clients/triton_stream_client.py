c2baaa674baefcbc2dac6c718dc06726e25477ab
yanhe13 <yanhe13@huawei.com>
2025-05-30 15:35:52 +0800

    full configs

--- ais-bench_workload/experimental_tools/mindie_benchmark/mindie_ais_bench_backend/clients/triton_stream_client.py
+++ ais-bench_workload/experimental_tools/mindie_benchmark/mindie_ais_bench_backend/clients/triton_stream_client.py
@@ -18,6 +18,9 @@ class TritonStreamClient(BaseStreamClient, ABC):
         generated_text = json_content.get("text_output", None)
         if generated_text:
             response.update({"generated_text": generated_text})
+            response.update({"generated_tokens": json_content["details"].get("generated_tokens", 1)})
+            response.update({"batch_size": json_content["details"].get("batch_size", 1)})
+            response.update({"queue_wait_time": json_content["details"].get("queue_wait_time", 1)})
         if self.do_performance:
             response.update({"token_str": generated_text})
         return response
@@ -31,15 +34,15 @@ class TritonStreamClient(BaseStreamClient, ABC):
         prefill_time = res.get("prefill_time")
         if prefill_time:
             inputs.prefill_latency = prefill_time
-            inputs.prefill_batch_size = res["details"].get("batch_size", 0)
+            inputs.prefill_batch_size = res.get("batch_size", 0)
         decode_time = res.get("decode_time")
         if decode_time:
             inputs.decode_cost.append(decode_time)
-            inputs.decode_batch_size.extend([res["details"].get("batch_size", 0)] * (res["details"].get("generated_tokens", 1) - self.last_generated_tokens))
+            inputs.decode_batch_size.extend([res.get("batch_size", 0)] * (res.get("generated_tokens", 1) - self.last_generated_tokens))
         chunk_time_point = res.get("chunk_time_point")
         if chunk_time_point:
             inputs.chunk_time_point_list.append(chunk_time_point)
         inputs.num_generated_tokens += 1
-        inputs.queue_wait_time.append(res["details"]["queue_wait_time"])
-        self.last_generated_tokens = res["details"].get("generated_tokens", 1)
+        inputs.queue_wait_time.append(res["queue_wait_time"])
+        self.last_generated_tokens = res.get("generated_tokens", 1)
         return generated_text

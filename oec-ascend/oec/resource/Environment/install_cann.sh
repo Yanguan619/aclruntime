60d00f0199aa302c019971a63fbcc17d6e8429e7
卤辣鲜香 <love@zerli.cn>
2025-06-04 02:31:06 +0000

    【oec-ascend】CANN安装拉起脚本输出ASCEND_HOME_PATH环境变量等参考信息

--- oec-ascend/oec/resource/Environment/install_cann.sh
+++ oec-ascend/oec/resource/Environment/install_cann.sh
@@ -5,6 +5,9 @@ if [ "$count" -ne 1 ]; then
     echo numer of $2 is not equal to 1
     exit 1
 fi
+echo ">>>>>>>>>>>> ASCEND_HOME_PATH <<<<<<<<<<<<<<<<<"
+env |grep ASCEND_HOME_PATH
+echo ">>>>>>>>>>>> INSTALL " ./$2* " <<<<<<<<<<<<<<<<<"
 echo ./$2* --install --quiet --install-path=$(realpath $1)/Ascend
 chmod +x $2*
 ./$2* --install --quiet --install-path=$(realpath $1)/Ascend

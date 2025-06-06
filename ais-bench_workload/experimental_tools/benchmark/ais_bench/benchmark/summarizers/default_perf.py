b64989915b9c0fed225a10da810fe36dfe477194
z30065766 <zhangxun79@h-partners.com>
2025-06-06 10:00:18 +0800

    【bugfix】修复无Decode请求在请求线段图中的显示问题

--- ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/summarizers/default_perf.py
+++ ais-bench_workload/experimental_tools/benchmark/ais_bench/benchmark/summarizers/default_perf.py
@@ -78,12 +78,11 @@ class DefaultPerfSummarizer:
                 with open(perf_details_file, 'r', encoding='utf-8') as file:
                     self.logger.info(f"Loading detail perf data of {model=} {dataset=} ...")
                     details_data = json.load(file)
-                    decode_token_latencies = details_data["requests"]["decode_token_latencies"]
-                    decode_stage_exist = any(len(sub_list) != 0 for sub_list in decode_token_latencies)
                     plot_file_path = osp.join(self.work_dir, "performances", model, f"{dataset}_plot.html")
                     plot_sorted_request_timelines(details_data["requests"]["start_time"],
                                                   details_data["requests"]["prefill_latency"],
-                                                  details_data["requests"]["end_time"] if decode_stage_exist else None,
+                                                  details_data["requests"]["end_time"],
+                                                  details_data["requests"]["decode_token_latencies"],
                                                   output_file=plot_file_path, unit="s")
                     self.logger.info(f"The {dataset}_plot has been saved in {plot_file_path}")
                 calculators_per_model[dataset] = build_perf_metric_calculator_from_cfg(calculator_conf)

db6cf5c83919a8f12c91ebbab5e286b6485e4c5c
卤辣鲜香 <love@zerli.cn>
2025-06-09 09:59:02 +0000

    【oec-ascend】优化AOE测试用例

--- oec-ascend/oec/resource/ModelDev/AOETool/ascend_test_aoe.py
+++ oec-ascend/oec/resource/ModelDev/AOETool/ascend_test_aoe.py
@@ -3,12 +3,8 @@ import oec
 oec.TestCase(
     group= ("模型开发","模型调优"),
     name = "MODEL_AOE_TF",
-    cmd=f"aoe --framework=3 --model={oec.Context.data_path}/model/model_tf.pb --job_type=2"
-    )
-oec.TestCase(
-    group= ("模型开发","模型调优"),
-    name = "MODEL_AOE_TF_CLEAN",
-    cmd=f"rm -rf aoe_workspace && rm -rf kernel_meta && rm -rf *.json",
-    auxiliary=True,
-    optional=True
+    cmd=f"aoe --framework=3 --model={oec.Context.data_path}/model/model_tf.pb --job_type=2\n"
+        "rm -rf aoe_workspace \n"
+        "rm -rf kernel_meta \n"
+        "rm -rf *.json"
     )

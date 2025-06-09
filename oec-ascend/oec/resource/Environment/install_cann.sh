aca30d28dc3700bb3c1aa2203a829804e50cb069
卤辣鲜香 <love@zerli.cn>
2025-06-09 09:58:43 +0000

    【oec-ascend】优化CANN安装用例

--- oec-ascend/oec/resource/Environment/install_cann.sh
+++ oec-ascend/oec/resource/Environment/install_cann.sh
@@ -1,14 +1,47 @@
 cd $1
-echo $1 $2
-count=$(find . -type f -name "$2*" | wc -l)
-if [ "$count" -ne 1 ]; then
-    echo numer of $2 is not equal to 1
-    exit 1
+function install(){
+    package=$1
+    count=$(find . -type f -name "$package*" | wc -l)
+    if [ "$count" -ne 1 ]; then
+        echo numer of $package is not equal to 1
+        exit 1
+    fi
+    echo ">>>>>>>>>>>> ASCEND_HOME_PATH <<<<<<<<<<<<<<<<<"
+    env |grep ASCEND_HOME_PATH
+    echo ">>>>>>>>>>>> INSTALL " ./$package* " <<<<<<<<<<<<<<<<<"
+    echo ./$package* --install --quiet --install-path=$(realpath $1)/Ascend
+    chmod +x $package*
+    ./$2* --install --quiet --install-path=$(realpath $1)/Ascend
+    if [[ $? != 0 ]]; then
+        exit $?
+    fi
+}
+function uninstall(){
+    package=$1
+    count=$(find . -type f -name "$package*" | wc -l)
+    if [ "$count" -ne 1 ]; then
+        echo numer of $package is not equal to 1
+        exit 1
+    fi
+    echo ./$package* --uninstall --install-path=$(realpath $1)/Ascend
+    chmod +x $package*
+    ./$2* --uninstall --install-path=$(realpath $1)/Ascend
+    if [[ $? != 0 ]]; then
+        exit $?
+    fi
+}
+
+install Ascend-cann-toolkit
+source Ascend/ascend-toolkit/set_env.sh
+install Ascend-cann-kernels
+install Ascend-cann-nnal
+uninstall Ascend-cann-nnal
+uninstall Ascend-cann-kernels
+uninstall Ascend-cann-toolkit
+
+code=0
+if [[ -d Ascend/ascend-toolkit ]]; then
+    code=1
 fi
-echo ">>>>>>>>>>>> ASCEND_HOME_PATH <<<<<<<<<<<<<<<<<"
-env |grep ASCEND_HOME_PATH
-echo ">>>>>>>>>>>> INSTALL " ./$2* " <<<<<<<<<<<<<<<<<"
-echo ./$2* --install --quiet --install-path=$(realpath $1)/Ascend
-chmod +x $2*
-./$2* --install --quiet --install-path=$(realpath $1)/Ascend
-exit $?
+rm -rf Ascend
+exit $code

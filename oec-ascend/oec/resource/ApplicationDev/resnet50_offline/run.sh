4a20919e436692f9811ec6bbf58cf23373267ae8
卤辣鲜香 <love@zerli.cn>
2025-06-10 09:34:27 +0000

    【oec-ascend】resnet50用例优化，临时文件保存到output目录

--- oec-ascend/oec/resource/ApplicationDev/resnet50_offline/run.sh
+++ oec-ascend/oec/resource/ApplicationDev/resnet50_offline/run.sh
@@ -1,15 +1,21 @@
-mkdir build
-cd build
 
-g++ ../resnet50.cpp -o resnet50 -lascendcl -ldl -lpthread -L${ASCEND_HOME_PATH}/lib64 -I${ASCEND_HOME_PATH}/include
+data=$1
+output=$2
+npu=$3
+mkdir -p "$output/tmp/resnet50/model"
+g++ resnet50.cpp -o "$output/tmp/resnet50/resnet50" -lascendcl -ldl -lpthread -L${ASCEND_HOME_PATH}/lib64 -I${ASCEND_HOME_PATH}/include
 if [[ $? != 0 ]]; then
     cd ..
     rm -rf build
     exit -1
 fi
-./resnet50 $1
+
+cd "$output/tmp/resnet50"
+atc --model="$data/model/resnet50.onnx" --framework=5 --output="model/resnet50" --input_shape="actual_input_1:1,3,224,224" --soc_version=$npu
+cp -r "$data/data" "$output/tmp/resnet50"
+
+./resnet50 "$output/tmp/resnet50"
 rst=$?
 echo rst=$rst
-cd ..
-rm -rf build
+
 exit $rst

b5445bb36f58caa09112a0da18ba321de84ede61
yanhe13 <yanhe13@huawei.com>
2025-06-12 15:07:50 +0800

    roll back def

--- ais-bench_workload/tool/ais_bench/test/cpp_test/test_pyinferencesession.cpp
+++ ais-bench_workload/tool/ais_bench/test/cpp_test/test_pyinferencesession.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: yanhe13 yanhe13@huawei.com
+ * @Date: 2025-06-12 14:56:37
+ * @LastEditors: yanhe13 yanhe13@huawei.com
+ * @LastEditTime: 2025-06-12 15:07:37
+ * @FilePath: \ais_bench\test\cpp_test\test_pyinferencesession.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <memory>
@@ -7,6 +15,9 @@
 using namespace Base;
 using ::testing::_;
 using ::testing::Return;
+
+// 保存原始File宏定义
+#pragma push_macro("File")
 
 // 由于File类使用静态方法，这里通过宏替换实现Mock
 #undef File
@@ -97,5 +108,5 @@ TEST_F(PyInferenceSessionTest, InvalidDeviceId) {
 }
 } // namespace AISBench_test
 
-// 测试完成后恢复原始File类
+// 恢复原始File宏定义
 #pragma pop_macro("File")

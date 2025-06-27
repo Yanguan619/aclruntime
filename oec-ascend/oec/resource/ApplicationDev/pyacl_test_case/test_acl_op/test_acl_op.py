98ed7f33e81181bd8f5d5eb67bea2bc676411a19
133******23 <825161726@qq.com>
2025-06-27 08:18:47 +0000

    自动转换单算子模型

--- oec-ascend/oec/resource/ApplicationDev/pyacl_test_case/test_acl_op/test_acl_op.py
+++ oec-ascend/oec/resource/ApplicationDev/pyacl_test_case/test_acl_op/test_acl_op.py
@@ -8,10 +8,19 @@ import numpy as np
 import acl
 import utils as util
 import sys
+import subprocess
 #from constant import Const
 
 data_path = sys.argv[1]
 print(f"data path is {data_path}")
+
+#get soc version
+soc_version = acl.get_soc_name()
+print(f"soc version is {soc_version}")
+
+#transfer op model
+subprocess.run(f"atc --singleop={data_path}/data/add.json --output={data_path}/model --soc_version={soc_version}", shell=True)
+print(f"ATC successed!")
 
 acl_dtype = {
     "float32": 0,

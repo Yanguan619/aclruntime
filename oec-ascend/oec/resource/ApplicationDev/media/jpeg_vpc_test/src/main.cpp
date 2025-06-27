f92fe8078d618fcd06b05124588b1d9a30b12ee4
卤辣鲜香 <love@zerli.cn>
2025-06-27 03:58:06 +0000

    【oec-ascend】ACL jpeg和vpc 测试用例

---
+++ oec-ascend/oec/resource/ApplicationDev/media/jpeg_vpc_test/src/main.cpp
@@ -0,0 +1,68 @@
+/**
+* @file main.cpp
+*
+* Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "sample_process.h"
+#include "utils.h"
+
+using namespace std;
+
+int main(int argc,const char *argv[])
+{
+    INFO_LOG("./main param, param represents a vpc feature and must be set");
+    if (argc != 4) {
+        ERROR_LOG("input param not be set");
+        return FAILED;
+    }
+    string data_path = string(argv[2]);
+    string output_path = string(argv[3]);
+    string result_path_str = output_path + "/result";
+    Result ret = Utils::CheckAndCreateFolder(result_path_str.c_str());
+    if (ret != SUCCESS) {
+        ERROR_LOG("mkdir out folder error, dir = %s.", result_path_str.c_str());
+        return FAILED;
+    }
+
+    SampleProcess sampleProcess;
+    ret = sampleProcess.InitResource();
+    if (ret != SUCCESS) {
+        ERROR_LOG("sample init resource failed");
+        return FAILED;
+    }
+
+    DvppType dvppType = static_cast<DvppType>(atoi(argv[1]));
+    if (dvppType == JPEG_ENCODE) {
+        ret = sampleProcess.JpegeProcess(dvppType);
+        if (ret != SUCCESS) {
+            ERROR_LOG("sample jpege process failed");
+            return FAILED;
+        }
+        return SUCCESS;
+    }
+
+    if (dvppType == VPC_8K_RESIZE) {
+        ret = sampleProcess.Resize8kProcess(dvppType);
+        if (ret != SUCCESS) {
+            ERROR_LOG("sample resize 8k process failed");
+            return FAILED;
+        }
+        return SUCCESS;
+    }
+
+    ret = sampleProcess.JpegdProcess(dvppType);
+    if (ret != SUCCESS) {
+        ERROR_LOG("sample model process failed");
+        return FAILED;
+    }
+
+    INFO_LOG("execute sample success");
+    return SUCCESS;
+}

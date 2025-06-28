59d2930a88eede4bb950ecf3abfd121afb76d8d3
133******23 <825161726@qq.com>
2025-06-28 06:52:30 +0000

    change model saved path

--- oec-ascend/oec/resource/ApplicationDev/pyacl_test_case/test_acl_op/test_acl_op.py
+++ oec-ascend/oec/resource/ApplicationDev/pyacl_test_case/test_acl_op/test_acl_op.py
@@ -15,6 +15,9 @@ import json
 data_path = sys.argv[1]
 print(f"data path is {data_path}")
 
+output_dir = sys.argv[2]
+print(f"output dir is {output_dir}")
+
 #get soc version
 soc_version = acl.get_soc_name()
 print(f"soc version is {soc_version}")
@@ -47,11 +50,11 @@ add_json="""
 """
 add_json = json.loads(add_json)
 
-with open(f"{data_path}/data/add.json", 'w', encoding='utf-8') as json_file:
+with open(f"{output_dir}/add.json", 'w', encoding='utf-8') as json_file:
     json.dump(add_json, json_file, ensure_ascii=False, indent=4)
 
 #transfer op model
-subprocess.run(f"atc --singleop={data_path}/data/add.json --output={data_path}/model --soc_version={soc_version}", shell=True)
+subprocess.run(f"atc --singleop={output_dir}/add.json --output={output_dir} --soc_version={soc_version}", shell=True)
 
 
 acl_dtype = {
@@ -343,7 +346,7 @@ class TestOp(unittest.TestCase):
         if ret:
             print("acl.init failed! ret:", ret)
             raise AssertionError
-        ret = acl.op.set_model_dir(f"{data_path}/model")
+        ret = acl.op.set_model_dir(f"{output_dir}")
         if ret:
             print("acl.op.set_model_dir failed! ret:", ret)
             raise AssertionError
@@ -356,7 +359,7 @@ class TestOp(unittest.TestCase):
         """
         test case for loading operator
         """
-        np_data = np.fromfile(f"{data_path}/model/0_Add_3_2_8_16_3_2_8_16_3_2_8_16.om", dtype="int8")
+        np_data = np.fromfile(f"{output_dir}/0_Add_3_2_8_16_3_2_8_16_3_2_8_16.om", dtype="int8")
         bytes_data = np_data.tobytes()
         buffer = acl.util.bytes_to_ptr(bytes_data)
         np_size = np_data.size 

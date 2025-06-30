fd6e9e260382fd962537c711d2f9d856c146033a
卤辣鲜香 <love@zerli.cn>
2025-06-30 12:38:01 +0000

    【oec-ascend】msopgen ADD算子开发用例

--- oec-ascend/oec/resource/KernelDev/AddKernelmsOpGen/ascend_test_kernel.py
+++ oec-ascend/oec/resource/KernelDev/AddKernelmsOpGen/ascend_test_kernel.py
@@ -2,7 +2,7 @@ import oec
 oec.TestCase(
     group= ("算子","算子开发"),
     name = "KERNEL_DEV_ADD_MSOPGEN",
-    cmd=f"bash run.sh -r cpu -v Ascend910B3 -o '{oec.Context.output_dir}/tmp/AddKernelmsOpGen'",
+    cmd=f"bash run.sh '{oec.Context.output_dir}/tmp/AddKernelmsOpGen'",
     exclude=None,
-    include="test pass"
+    include="successfully created"
     )

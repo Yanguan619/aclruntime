8800012c34d1d783747da77f35468e65e5ef6ca8
卤辣鲜香 <love@zerli.cn>
2025-06-30 06:55:00 +0000

    【oec-ascend】算子开发编译用例编号调整

--- oec-ascend/oec/resource/KernelDev/tik_matmul/ascend_test_tbe.py
+++ oec-ascend/oec/resource/KernelDev/tik_matmul/ascend_test_tbe.py
@@ -3,6 +3,6 @@ import oec
 
 oec.TestCase(
     group= ("算子","算子编译"),
-    name = "KERNEL_DEV_TIK_MATMUL",
+    name = "KERNEL_BUILD_TIK_MATMUL",
     cmd=f"python3 tik_matmul.py {oec.Context.output_dir}/tmp/tik_matmul"
     )

6ae05a5af765a7c1f2d3b5a47ce2f8aaad5d68e1
卤辣鲜香 <love@zerli.cn>
2025-06-30 09:53:53 +0000

    【oec-ascend】ADD算子开发用例适配框架

--- oec-ascend/oec/resource/KernelDev/AddKernelInvocationNeo/ascend_test_kerneldev.py
+++ oec-ascend/oec/resource/KernelDev/AddKernelInvocationNeo/ascend_test_kerneldev.py
@@ -3,17 +3,23 @@ import oec
 oec.NPUTestCase(
     group= ("算子","算子开发"),
     name = "KERNEL_DEV_ADD_NPU",
-    cmd=f"bash run.sh -r npu -v <NPU> -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'"
+    cmd=f"bash run.sh -r npu -v <NPU> -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'",
+    exclude=None,
+    include="test pass"
     )
 
 oec.NPUTestCase(
     group= ("算子","算子开发"),
-    name = "KERNEL_BUILD_ADD_SIM",
-    cmd=f"bash run.sh -r sim -v <NPU> -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'"
+    name = "KERNEL_DEV_ADD_SIM",
+    cmd=f"bash run.sh -r sim -v <NPU> -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'",
+    exclude=None,
+    include="test pass"
     )
 
 oec.TestCase(
     group= ("算子","算子开发"),
-    name = "KERNEL_BUILD_ADD_CPU",
-    cmd=f"bash run.sh -r cpu -v Ascend910B3 -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'"
+    name = "KERNEL_DEV_ADD_CPU",
+    cmd=f"bash run.sh -r cpu -v Ascend910B3 -o '{oec.Context.output_dir}/tmp/AddKernelInvocationNeo'",
+    exclude=None,
+    include="test pass"
     )

68e5073850aaf22c9965aede835f6249777aad14
卤辣鲜香 <love@zerli.cn>
2025-07-03 08:30:44 +0000

    【oec-ascend】CANN安装卸载适配A3包名

--- oec-ascend/oec/resource/Environment/install_cann_packages.sh
+++ oec-ascend/oec/resource/Environment/install_cann_packages.sh
@@ -9,7 +9,7 @@ mkdir -p "$install_path"
 
 function install(){
     package=$1
-    count=$(find . -type f -name "$package*" | wc -l)
+    count=$(find . -type f -name "*$package*" | wc -l)
     if [ "$count" -ne 1 ]; then
         echo "ERROR: numer of $package is not equal to 1"
         exit 1
@@ -19,8 +19,8 @@ function install(){
     echo ">>>>>>>>>>>> ASCEND_HOME_PATH <<<<<<<<<<<<<<<<<"
     env |grep ASCEND_HOME_PATH
     echo  ===============================================
-    echo ./$package* --install --install-path="$install_path" --quiet
-    chmod +x $package*
+    echo ./*$package* --install --install-path="$install_path" --quiet
+    chmod +x *$package*
     ./$package* --install --quiet --install-path="$install_path"
     if [[ $? != 0 ]]; then
         exit $?
@@ -28,41 +28,41 @@ function install(){
 }
 function uninstall(){
     package=$1
-    count=$(find . -type f -name "$package*" | wc -l)
+    count=$(find . -type f -name "*$package*" | wc -l)
     if [ "$count" -ne 1 ]; then
         echo "ERROR: numer of $package is not equal to 1"
         exit 1
     fi
-    echo ./$package* --uninstall --install-path="$install_path"
-    chmod +x $package*
+    echo ./*$package* --uninstall --install-path="$install_path"
+    chmod +x *$package*
     ./$package* --uninstall --install-path="$install_path"
     if [[ $? != 0 ]]; then
         exit $?
     fi
 }
 
-install Ascend-cann-toolkit
+install cann-toolkit
 if [[ $? != 0 ]]; then
     exit $?
 fi
 source ${install_path}/ascend-toolkit/set_env.sh
-install Ascend-cann-kernels
+install cann-kernels
 if [[ $? != 0 ]]; then
     exit $?
 fi
-install Ascend-cann-nnal
+install cann-nnal
 if [[ $? != 0 ]]; then
     exit $?
 fi
-uninstall Ascend-cann-nnal
+uninstall cann-nnal
 if [[ $? != 0 ]]; then
     exit $?
 fi
-uninstall Ascend-cann-kernels
+uninstall cann-kernels
 if [[ $? != 0 ]]; then
     exit $?
 fi
-uninstall Ascend-cann-toolkit
+uninstall cann-toolkit
 if [[ $? != 0 ]]; then
     exit $?
 fi

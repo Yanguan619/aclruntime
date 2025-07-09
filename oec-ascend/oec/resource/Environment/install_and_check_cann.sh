8f52dcac9b8520639c43410c85453b5520a71493
卤辣鲜香 <love@zerli.cn>
2025-07-09 07:18:35 +0000

    【oec-ascend】CANN安装卸载bugfix

--- oec-ascend/oec/resource/Environment/install_and_check_cann.sh
+++ oec-ascend/oec/resource/Environment/install_and_check_cann.sh
@@ -2,6 +2,8 @@ if [[ -f /etc/Ascend/ascend_cann_install.info ]]; then
     mv /etc/Ascend/ascend_cann_install.info /etc/Ascend/ascend_cann_install.info.bac
 fi
 bash install_cann_packages.sh "$1" "$2"
+rst=$?
 if [[ -f /etc/Ascend/ascend_cann_install.info.bac ]]; then
     mv /etc/Ascend/ascend_cann_install.info.bac /etc/Ascend/ascend_cann_install.info
 fi
+exit $rst

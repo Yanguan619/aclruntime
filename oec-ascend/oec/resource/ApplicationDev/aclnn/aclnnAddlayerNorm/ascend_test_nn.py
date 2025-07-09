577c3240ac7d9ee65a438f0bbf25d07abd662def
卤辣鲜香 <love@zerli.cn>
2025-07-09 07:47:58 +0000

    【oec-ascend】测试用例拉起脚本优化，去除不必要的参数

--- oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAddlayerNorm/ascend_test_nn.py
+++ oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAddlayerNorm/ascend_test_nn.py
@@ -5,9 +5,7 @@ oec.TestCase(
     group= ("应用开发","算子加速库"),
     
     name = "ACLNN_ADDLAYERNORM",
-    cmd=f"./run.sh {oec.Context.output_dir}/tmp/aclnn",
-    expect=0,
-    exclude=['failed','Failed','FAILED','error','ERROR','Error']
+    cmd=f"./run.sh {oec.Context.output_dir}/tmp/aclnn"
     )
 
 

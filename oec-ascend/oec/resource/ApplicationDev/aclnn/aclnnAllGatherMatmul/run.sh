087c7d016b579aef644e0779bef11a4b067f8aab
卤辣鲜香 <love@zerli.cn>
2025-07-11 09:12:12 +0000

    【oec-ascend】aclnnAllGatherMatMul用例适配环境的device数量

--- oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAllGatherMatmul/run.sh
+++ oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAllGatherMatmul/run.sh
@@ -1,8 +1,10 @@
 output_path="$1/aclnnAllGatherMatmul"
+device_num=$2
 src_path=$(pwd)
 mkdir -p "$output_path" 
 cd "${output_path}"
-cmake "$src_path" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_SKIP_RPATH=TRUE
+#device_num 是环境上npu的数量，当前测试在A2上需要为2,4,8时才能运行成功
+cmake "$src_path" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_SKIP_RPATH=TRUE -DDEV_NUM=${device_num}
 make
 
 cd bin

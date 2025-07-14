cfbc1c395a063047cb717bc56c20bf5b5c44bb33
卤辣鲜香 <love@zerli.cn>
2025-07-14 02:47:23 +0000

    【oec-ascend】aclnnAllGatherMatMul用例值匹配error

--- oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAllGatherMatmul/ascend_test_nn.py
+++ oec-ascend/oec/resource/ApplicationDev/aclnn/aclnnAllGatherMatmul/ascend_test_nn.py
@@ -5,7 +5,8 @@ oec.NPUTestCase(
     group= ("应用开发","算子加速库"),
     
     name = "ACLNN_ALLGATHERMATMUL",
-    cmd=f"./run.sh {oec.Context.output_dir}/tmp/aclnn <Count>"
+    cmd=f"./run.sh {oec.Context.output_dir}/tmp/aclnn <Count>",
+    exclude=[r"\berror\b", r"\bERROR\b", r"\bError\b"]
     )
 
 

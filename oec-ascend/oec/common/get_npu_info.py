4d49d8f66a8a2d1b01a775a834fa6c5e441874cc
卤辣鲜香 <love@zerli.cn>
2025-08-01 09:27:43 +0000

    【oec-ascend】部分用例提取到公共区域

---
+++ oec-ascend/oec/common/get_npu_info.py
@@ -0,0 +1,9 @@
+try:
+    import acl
+    print(acl.get_soc_name())
+    Count,ret = acl.rt.get_device_count()
+    if ret !=0:
+        exit(1)
+    print(Count,end='')
+except Exception as e:
+    exit(2)

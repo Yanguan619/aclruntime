d6f32a533a9f99be15e7c4b0c2beaed0d395b784
卤辣鲜香 <love@zerli.cn>
2025-08-06 09:17:41 +0000

    【oec-ascend】算子开发用例归一

---
+++ oec-ascend/oec/test_cases/cann/Kernel/Develop/KERNEL_DEV_ADD/TEST_NPU.sh
@@ -0,0 +1,9 @@
+cd AddKernelInvocationNeo
+SOC_VERSION=$(python3 -c "
+try:
+    import acl
+    print(acl.get_soc_name())
+except:
+    print('unknow')
+")
+bash run.sh -r npu -v $SOC_VERSION -o "$OEC_OUTPUT_PATH"

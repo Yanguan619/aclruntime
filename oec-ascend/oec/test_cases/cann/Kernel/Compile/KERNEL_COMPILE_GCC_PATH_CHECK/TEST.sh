e0d8fe97a27f61fffe3604df011b6b0fb0f89827
卤辣鲜香 <love@zerli.cn>
2025-08-18 09:02:43 +0000

    【oec-ascend】添加gcc安装路径检查用例

---
+++ oec-ascend/oec/test_cases/cann/Kernel/Compile/KERNEL_COMPILE_GCC_PATH_CHECK/TEST.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 使用数组直接存储路径（避免空格问题）
+paths=(
+    "/usr/lib/gcc/x86_64-linux-gnu/"
+    "/usr/lib/gcc/aarch64-linux-gnu"
+    
+    "/usr/lib/gcc/x86_64-openEuler-gnu"
+    "/usr/lib/gcc/aarch64-openEuler-gnu"
+)
+
+for path in "${paths[@]}"; do
+    if [[ -e "$path" ]]; then
+        echo "存在路径: $path"
+        exit 0
+    fi
+done
+
+echo "所有路径均不存在"
+exit 255

d7dd9da60ecf2eaf333c2db13534fc7a0232bd0e
卤辣鲜香 <love@zerli.cn>
2025-09-10 19:03:08 +0800

    【oec-ascend】去掉A3

--- oec-ascend/oec/test_cases/cann/ApplicationDev/Basic/SAMPLE_ASCEND_GRAPH_IR_BUILD/TEST.sh
+++ oec-ascend/oec/test_cases/cann/ApplicationDev/Basic/SAMPLE_ASCEND_GRAPH_IR_BUILD/TEST.sh
@@ -1,3 +1,10 @@
+
+unsupported=(A3)
+for product in "${unsupported[@]}"; do
+    if [[ "$product" == "$OEC_PRODUCT" ]]; then
+        exit 192
+    fi
+done
 set -e
 mkdir -p "$OEC_OUTPUT_PATH"
 current_dir=$(pwd)/IRBuild

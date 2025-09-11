c0eefc0eff3041daae5291d5e1779d120052a3f3
卤辣鲜香 <love@zerli.cn>
2025-09-11 11:25:25 +0800

    【oec-ascend】MSOPGen问题修复

--- oec-ascend/oec/test_cases/cann/Kernel/Develop/KERNEL_DEV_ADD_MSOPGEN/TEST.sh
+++ oec-ascend/oec/test_cases/cann/Kernel/Develop/KERNEL_DEV_ADD_MSOPGEN/TEST.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 output_path="$OEC_OUTPUT_PATH"
-msopgen gen -i add_custom.json -c ai_core-Ascend910B3 -lan cpp -out "$output_path"
+special=(A300)
+npu=$(python3 -c "
+try:
+    import acl
+    print(acl.get_soc_name())
+except:
+    print('unknow')
+")
+target=ai_core-Ascend910B3
+for product in "${special[@]}"; do
+    if [[ "$product" == "$OEC_PRODUCT" ]]; then
+        target="ai_core-$npu"
+    fi
+done
+msopgen gen -i add_custom.json -c $target -lan cpp -out "$output_path"
 cd "$output_path"
 bash build.sh
